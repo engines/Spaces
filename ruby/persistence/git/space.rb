7bc625fa3e32922219f3b8879cf7d7083b5451fa
Mark Ratjens <mark@ratjens.com>
2019-12-11 17:56:55 +1100

    identity Persistence::Git module
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

---
+++ ruby/persistence/git/space.rb
@@ -0,0 +1,13 @@
+require 'git'
+
+require_relative '../framework/space'
+
+module Persistence
+  module Git
+    class Space < ::Framework::Space
+      # The dimensions in which Git repos exist
+
+
+    end
+  end
+end
