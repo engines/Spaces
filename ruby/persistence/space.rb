5e6f621628a52b37b7c5c6019dbbd5975dabe1e7
Mark Ratjens <mark@ratjens.com>
2019-12-12 15:34:39 +1100

    define git and uri spaces within persistence space
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- ruby/persistence/space.rb
+++ ruby/persistence/space.rb
@@ -1,8 +1,23 @@
 require_relative '../framework/space'
+require_relative 'git/space'
+require_relative 'uri/space'
 
 module Persistence
   class Space < ::Framework::Space
     # The dimensions in which long-lived recordings of software objects can be made and maintained.
+
+    class << self
+      def git
+        @@git ||= Persistence::Git::Space.new
+      end
+
+      def uri
+        @@uri ||= Persistence::Uri::Space.new
+      end
+    end
+
+    def_delegator self, :git
+    def_delegator self, :uri    
 
   end
 end
