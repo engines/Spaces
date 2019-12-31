11bd8452b103850f6e00104f4a0717f71de57112
Mark Ratjens <mark@ratjens.com>
2019-12-31 16:14:24 +1100

    surface #by(descriptor) and #import(descriptor) in Blueprint::Space

--- ruby/blueprint/space.rb
+++ ruby/blueprint/space.rb
@@ -1,15 +1,19 @@
 require_relative '../blueprint/blueprint'
-require_relative '../framework/space'
+require_relative '../universal/space'
 
 module Blueprint
   class Space < ::Framework::Space
     # The dimensions in which blueprints exist
 
     def by(descriptor)
+      universal.persistence.by(descriptor)
     end
 
-    def encloses(descriptor)
+    def encloses?(blueprint)
     end
 
+    def import(descriptor)
+      universal.persistence.import(descriptor)
+    end
   end
 end
