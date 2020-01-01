7d4205e74378657df9cd15b7e5f3b17b104015e7
Mark Ratjens <mark@ratjens.com>
2020-01-01 12:26:14 +1100

    use OpenStruct for model memo strategy

--- ruby/framework/model.rb
+++ ruby/framework/model.rb
@@ -25,44 +25,18 @@ module Framework
       end
     end
 
+    attr_accessor :struct
+
     def identifier
       self.class.identifier
     end
 
-    def to_memo
-      to_h(memo_variables)
-    rescue SystemStackError => e
-    end
-
     def to_yaml
-      YAML.dump(to_memo)
-    end
-
-    def memo_variables
-      []
+      YAML.dump(struct)
     end
 
     def open_struct_from_json(json)
       JSON.parse(json, object_class: OpenStruct)
-    end
-
-    private
-
-    def to_h(variables)
-      variables.each_with_object({}) do |i, h|
-        ivv = instance_variable_get(i)
-
-        h[i.to_s.delete('@')] =
-          if ivv.is_a? Model
-            ivv.to_memo
-          elsif ivv.is_a? Array
-            ivv.map { |v| v.respond_to?(:to_h) ? v.to_h : v }
-          elsif ivv.is_a? Hash
-            ivv.transform_values { |v| v.respond_to?(:to_h) ? v.to_h : v }
-          else
-            ivv
-          end
-      end
     end
   end
 end
