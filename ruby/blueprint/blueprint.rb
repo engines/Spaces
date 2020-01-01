300e1b1be8a2c69f39d74608a27ffc8c02339b31
Mark Ratjens <mark@ratjens.com>
2020-01-01 17:45:21 +1100

    save and retieve blueprints as yaml in Blueprint::Space

--- ruby/blueprint/blueprint.rb
+++ ruby/blueprint/blueprint.rb
@@ -4,14 +4,12 @@ require_relative '../executable/tensor'
 require_relative '../container/tensor'
 require_relative '../software/tensor'
 require_relative '../image/tensor'
-require_relative '../universal/descriptor'
 
 module Blueprint
   class Blueprint < ::Framework::Model
     # A plan for making software executable and usable, potentially many times over
 
-    relation_accessor :descriptor,
-      :software_version,
+    relation_accessor :software_version,
       :executable_tensor,
       :container_tensor,
       :software_tensor,
--- ruby/blueprint/space.rb
+++ ruby/blueprint/space.rb
@@ -5,18 +5,37 @@ module Blueprint
   class Space < ::Framework::Space
     # The dimensions in which blueprints exist
 
-    def encloses?(blueprint)
-      persistence.encloses(blueprint.descriptor)
+    def new_for(descriptor)
+      model_class.new(open_struct_from_json(persistence.by(descriptor))).tap do |m|
+        m.struct.descriptor = descriptor
+      end
     end
 
     def by(descriptor)
-      model_class.new(open_struct_from_json(persistence.by(descriptor))).tap do |m|
-        m.descriptor = descriptor
+      f = File.open(file_name_for(descriptor), 'r')
+      begin
+        model_class.new(model_class.from_yaml(f.read))
+      ensure
+        f.close
       end
+    end
+
+    def save(model)
+      f = File.open(file_name_for(model.descriptor), 'w')
+      begin
+        f.write(model.to_yaml)
+      ensure
+        f.close
+      end
+    end
+
+    def file_name_for(descriptor)
+      "#{path}/#{descriptor.identifier}.yaml"
     end
 
     def import(descriptor)
       persistence.import(descriptor)
+      new_for(descriptor).tap { |m| save(m) }
     end
 
     def model_class
