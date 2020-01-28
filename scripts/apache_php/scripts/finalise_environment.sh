7306f07a0f93c410a2d3fb010204e52c56b9f37f
Mark Ratjens <mark@ratjens.com>
2020-01-28 13:21:30 +1100

    for now, remove scripts and other configuration files

--- scripts/apache_php/scripts/finalise_environment.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir  -p /var/log/apache2/ 
-chown www-data /var/log/apache2/ 
-mkdir  -p /run/apache2/ 
-chown www-data /run/apache2/
--- scripts/apache_php/scripts/install_pecl_mods.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-    for mod in $*
-     do
-     mod=`echo $mod | sed "/[;&]/s///g"`
-    echo " " |  pecl install $mod
-     done
-   
- 
