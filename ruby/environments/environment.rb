a62aaaf2374728ee92089737f1d324c13e35d676
Mark Ratjens <mark@ratjens.com>
2020-03-04 17:27:11 +1100

    allow access to environment variable values by method call

--- ruby/environments/environment.rb
+++ ruby/environments/environment.rb
@@ -13,9 +13,21 @@ module Environments
       end
     end
 
+    def variables
+      struct.variables
+    end
+
+    def variable(name)
+      variables.detect { |v| v.name == name.to_s }
+    end
+
     def initialize(tensor)
       super
       self.struct = tensor.struct.environment
+    end
+
+    def method_missing(m, *args, &block)
+      (v = variable(m)) ? v.value : super
     end
 
   end
