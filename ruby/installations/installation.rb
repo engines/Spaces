05bbf1d62d6a5755470f2d2c9edc49a6abaf49a4
Mark Ratjens <mark@ratjens.com>
2020-03-27 12:53:49 +1100

    use delegation in Installation

--- ruby/installations/installation.rb
+++ ruby/installations/installation.rb
@@ -58,17 +58,16 @@ module Installations
       end
     end
 
-    def installation
-      itself
-    end
-
-    def home_app_path
-      descriptor.home_app_path
-    end
-
-    def identifier
-      descriptor.identifier
-    end
+    delegate(
+      [
+        :all_classes,
+        :product_classes,
+        :installation_classes,
+        :mutable_divisions
+      ] => :klass,
+      installation: :itself,
+      [:identifier, :home_app_path] => :descriptor
+    )
 
     def product
       struct.tap do |s|
@@ -84,24 +83,8 @@ module Installations
       universe.blueprints.file_names_for(directory, descriptor)
     end
 
-    def all_classes
-      self.class.all_classes
-    end
-
-    def product_classes
-      self.class.product_classes
-    end
-
-    def installation_classes
-      self.class.installation_classes
-    end
-
-    def mutable_divisions
-      self.class.mutable_divisions
-    end
-
     def blueprint_label_for(key)
-      self.class.blueprint_map[key] || key
+      klass.blueprint_map[key] || key
     end
 
     def collaborators
