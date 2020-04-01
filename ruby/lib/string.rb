c8c9c451698b43ef2ffb7fb1da6282f138b0bc64
Mark Ratjens <mark@ratjens.com>
2020-04-01 17:01:29 +1100

    REFACTOR: Universal::Space

---
+++ ruby/lib/string.rb
@@ -0,0 +1,10 @@
+class String
+
+  def camelize
+    s = sub(/^[a-z\d]*/) { |match| match.capitalize }
+    s.gsub!(/(?:_|(\/))([a-z\d]*)/i) { "#{$1}#{$2.capitalize}" }
+    s.gsub!("/", "::")
+    s
+  end
+
+end
