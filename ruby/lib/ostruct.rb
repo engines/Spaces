e1bd12aedbd728f183cfb4f0ec93dc7048c2c733
Mark Ratjens <mark@ratjens.com>
2020-04-06 14:03:02 +1000

    implement OpenStruct#deep_to_hash

--- ruby/lib/ostruct.rb
+++ ruby/lib/ostruct.rb
@@ -11,4 +11,19 @@ class OpenStruct
     OpenStruct.new(self.to_h.reverse_merge(other&.to_h))
   end
 
+  def deep_to_h
+    to_h.transform_values do |v|
+      case v
+      when OpenStruct
+        v.deep_to_h
+      when Array
+        v.map(&:deep_to_h)
+      else
+        v
+      end
+    rescue NoMethodError
+      v
+    end
+  end
+
 end
