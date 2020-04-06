c93547893ffbb13259a00e76af5af2657c2b2944
Mark Ratjens <mark@ratjens.com>
2020-04-06 15:28:20 +1000

    be able to save models as json

--- ruby/spaces/space.rb
+++ ruby/spaces/space.rb
@@ -40,6 +40,15 @@ module Spaces
       end
     end
 
+    def save_json(model)
+      f = File.open("#{writing_name_for(model)}.json", 'w')
+      begin
+        f.write(model.struct.deep_to_h.to_json)
+      ensure
+        f.close
+      end
+    end
+
     def save_tar(model)
       %x(cd #{path_for(model)}; tar -czf #{path_for(model)}.tgz . 2>&1)
     end
