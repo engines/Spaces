ce8e7e9924933f0be2975927071d86c94e4831f3
Mark Ratjens <mark@ratjens.com>
2020-04-20 11:46:19 +1000

    build an image named after the installation identifier

--- ruby/docker/images/space.rb
+++ ruby/docker/images/space.rb
@@ -6,7 +6,10 @@ module Docker
   module Images
     class Space < ::Spaces::Space
 
+      relation_accessor :abstract
+
       delegate([:all, :get] => :bridge)
+      delegate(path_for: :abstract)
 
       def pull(identifier)
         bridge.create('fromImage' => identifier)
@@ -14,16 +17,38 @@ module Docker
 
       alias_method :import, :pull
 
-      def from_directory(path, &block)
-        bridge.build_from_dir(path, { 'dockerfile' => Files::File.identifier }, &block)
+      def from_subject(subject)
+        bridge.build_from_dir(path_for(subject), options_for(subject)).tap do |i|
+          i.tag('repo' => subject.identifier)
+        end
       end
 
       def from_tar(path)
-        bridge.build_from_tar(File.open("#{path}.tar", 'r'))
+        bridge.build_from_tar(file_class.open("#{path_for(subject)}.tar", 'r'))
+      end
+
+      def options_for(subject)
+        default_options
+      end
+
+      def default_options
+        {
+          'dockerfile' => file_class.identifier,
+          'force' => true,
+          'rm' => true
+        }
+      end
+
+      def initialize(abstract)
+        self.abstract = abstract
       end
 
       def bridge
         Docker::Image
+      end
+
+      def file_class
+        Files::File
       end
 
     end
