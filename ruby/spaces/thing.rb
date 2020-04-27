5a999a2cf256e4c3cd5e506f7ff40b133963d03a
Mark Ratjens <mark@ratjens.com>
2020-04-27 15:36:15 +1000

    use define_method for one-liners im Model & Thing

--- ruby/spaces/thing.rb
+++ ruby/spaces/thing.rb
@@ -27,13 +27,22 @@ module Spaces
 
     attr_accessor :struct, :klass
 
-    delegate([:identifier, :namespace, :qualifier] => :klass)
+    delegate(
+      [:identifier, :namespace, :qualifier] => :klass,
+      to_h: :struct
+    )
+
+    alias_method :context_identifier, :qualifier
 
     define_method (:klass) { @klass ||= self.class }
     define_method (:to_yaml) { YAML.dump(struct) }
     define_method (:to_json) { struct&.deep_to_h&.to_jsonn }
     define_method (:open_struct_from_json) { |j| JSON.parse(j, object_class: OpenStruct) }
     define_method (:to_s) { identifier }
+
+    def initialize(struct: nil)
+      self.struct = struct
+    end
 
   end
 end
