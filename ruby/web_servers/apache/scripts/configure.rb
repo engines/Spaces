498408996e8237b83c74109d31dcd164699892dc
Mark Ratjens <mark@ratjens.com>
2020-05-27 10:55:46 +1000

    get the domain’s fqdn correctly when in the web server’s scripts

--- ruby/web_servers/apache/scripts/configure.rb
+++ ruby/web_servers/apache/scripts/configure.rb
@@ -13,7 +13,7 @@ module WebServers
 
         def install_php_config
           %Q(
-          if test -f /home/engines/injections/framework/php/custom.ini 
+          if test -f /home/engines/injections/framework/php/custom.ini
            then
              cp /home/engines/injections/framework/php/custom.ini /etc/php/7.?/apache2/conf.d/
            fi
@@ -30,7 +30,7 @@ module WebServers
 
         def extra_config
           %Q(
-           if test -f /home/engines/injections/webserver/apache/extra.conf 
+           if test -f /home/engines/injections/webserver/apache/extra.conf
              then
               cp /home/engines/injections/webserver/apache/extra.conf /etc/apache2/conf-enabled/
            fi
@@ -41,7 +41,7 @@ module WebServers
           %Q(
           www_dir=#{context.root_directory}
           APACHE_LOG_DIR=/var/log/
-          echo  ServerName #{context.stage.resolution.domain.fqdn} > /etc/apache2/sites-enabled/000-default.conf
+          echo  ServerName #{context.stage.release.domain.fqdn} > /etc/apache2/sites-enabled/000-default.conf
           cat $SRC_FILE | while read LINE
            do
             eval echo \\"$LINE\\" >> /etc/apache2/sites-enabled/000-default.conf
