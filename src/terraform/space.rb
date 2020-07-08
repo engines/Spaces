d3c3739fab03a9fee0b56396759cd0af9cf3d879
Mark Ratjens <mark@ratjens.com>
2020-07-08 15:47:57 +1000

    provide terraform space calls to init, plan and apply

--- src/terraform/space.rb
+++ src/terraform/space.rb
@@ -3,12 +3,25 @@ require_relative '../spaces/space'
 
 module Terraform
   class Space < ::Spaces::Space
-    extend RubyTerraform
 
     def save(model)
       model.components.each { |t| save_text(t) }
       _save(model, content: model.content, as: :tf)
     end
 
+    def init(model); execute(:init, model) ;end
+    def plan(model); execute(:plan, model) ;end
+    def apply(model); execute(:apply, model) ;end
+
+    protected
+
+    def execute(command, model)
+      Dir.chdir(path_for(model))
+      bridge.send(command)
+    rescue RubyTerraform::Errors::ExecutionError => e
+      warn(error: e, descriptor: model.descriptor, verbosity: [:error])
+    end
+
+    def bridge; RubyTerraform ;end
   end
 end
