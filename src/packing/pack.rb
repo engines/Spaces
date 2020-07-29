52f0c973390a09a67818ee51218068ab003b07b3
Mark Ratjens <mark@ratjens.com>
2020-07-29 17:39:31 +1000

    REFACTOR: after last commit

--- src/packing/pack.rb
+++ src/packing/pack.rb
@@ -4,32 +4,26 @@ module Packing
   class Pack < ::Releases::Release
 
     delegate(
-      [:identifier, :builders] => :resolution
+      [:identifier, :builders, :client] => :resolution
     )
 
     alias_accessor :resolution, :predecessor
     alias_method :context_identifier, :identifier
     alias_method :keys, :schema_keys
+    alias_method :super_memento, :memento
 
-    def export
-      memento.merge(struct_for(builders.all.map(&:export)))
-    end
+    def repository_name; "#{client.identifier}/#{identifier}" ;end
 
-    def commit
-      memento.merge(struct_for(builders.all.map(&:commit)))
-    end
+    def export; output_with(:export) ;end
+    def commit; output_with(:commit) ;end
+    def memento; output_with(:memento) ;end
 
-    def memento
-      super.merge(struct_for(builders.all.map(&:memento)))
-    end
+    def output_with(method); super_memento.merge(struct_for(builders.all.map(&method))) ;end
+    def struct_for(builders); OpenStruct.new(builders: builders) ;end
 
     def initialize(resolution)
-      self.struct = OpenStruct.new(builders: resolution.memento.builders)
+      self.struct = struct_for(resolution.memento.builders)
       self.resolution = resolution
-    end
-
-    def struct_for(builders)
-      OpenStruct.new(builders: builders)
     end
 
   end
