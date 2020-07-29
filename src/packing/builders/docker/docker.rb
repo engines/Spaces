c9baddd3f439685aa1c9e7809219fb8b4dd185d2
Mark Ratjens <mark@ratjens.com>
2020-07-29 10:47:58 +1000

    export and commit packs

--- src/packing/builders/docker/docker.rb
+++ src/packing/builders/docker/docker.rb
@@ -5,6 +5,14 @@ module Packing
     module Docker
       class Docker < Builder
 
+        def export
+          memento.tap { |m| m[:export_path] = "#{identifier}.tar" }
+        end
+
+        def commit
+          memento.tap { |m| m[:commit] = true }
+        end
+
       end
     end
   end
