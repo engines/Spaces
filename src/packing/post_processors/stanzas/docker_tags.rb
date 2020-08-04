79c01874a1cc0fb8462091b3859f136165657c91
Mark Ratjens <mark@ratjens.com>
2020-08-04 15:05:33 +1000

    run docker-tags post-processor only for docker images

--- src/packing/post_processors/stanzas/docker_tags.rb
+++ src/packing/post_processors/stanzas/docker_tags.rb
@@ -3,16 +3,24 @@ require_relative '../../../releases/stanza'
 module Packing
   module PostProcessors
     module Stanzas
-      class Tags < ::Releases::Stanza
+      class DockerTags < ::Releases::Stanza
 
         def to_h
-          collaboration.images.all.map do |b|
+          matching_images.map do |b|
             {
               type: "#{b.type}-tag",
               repository: collaboration.repository_name,
               tags: 'latest'
             }
           end
+        end
+
+        def matching_images
+          collaboration.images.all.select { |i| matching_types.include?(i.type) }
+        end
+
+        def matching_types
+          [:docker]
         end
 
       end
