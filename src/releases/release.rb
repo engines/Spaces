419356a9a2cefceb8cc83bb4365a24cb8b23a338
Mark Ratjens <mark@ratjens.com>
2020-08-17 14:54:04 +1000

    at least for now, remove Stages
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- src/releases/release.rb
+++ src/releases/release.rb
@@ -1,23 +1,9 @@
 require_relative 'collaboration'
-require_relative 'stage'
 
 module Releases
   class Release < Collaboration
-    relation_accessor :stages
 
     delegate(blueprints: :universe)
-
-    def divisions
-      @divisions ||= [stages&.map(&:divisions), super].flatten.compact
-    end
-
-    def stages
-      @stages ||= struct.stages&.map { |s| stage_for(s) }
-    end
-
-    def stage_for(struct)
-      stage_class.new(struct: struct, release: self)
-    end
 
     def descriptors_for(division_identifier)
       descriptors_structs_for(division_identifier).map { |d| descriptor_class.new(d) }
@@ -31,12 +17,7 @@ module Releases
       blueprints.file_names_for(directory, context_identifier)
     end
 
-    def memento_for(key)
-      key == :stages ? stages.map(&:memento) : super
-    end
-
     def text_class; Texts::FileText ;end
-    def stage_class; Stage ;end
 
   end
 end
