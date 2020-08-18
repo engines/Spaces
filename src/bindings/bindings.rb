c4ed9fa572ef2a784e93857bf29d512de92a4381
Mark Ratjens <mark@ratjens.com>
2020-08-18 14:40:55 +1000

    convenience method Bindings#resolutions
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- src/bindings/bindings.rb
+++ src/bindings/bindings.rb
@@ -4,7 +4,13 @@ require_relative 'binding'
 module Bindings
   class Bindings < ::Releases::Division
 
-    def named(name); all.detect { |b| b.identifier == name.to_s } ;end
+    def named(name)
+      all.detect { |b| b.identifier == name.to_s }
+    end
+
+    def resolutions
+      all.map(&:resolution)
+    end
 
     def method_missing(m, *args, &block); named(m) || super ;end
     def respond_to_missing?(m, *); named(m) || super ;end
