e608ea5dfde024c56b61bc9f6e02adf50f06d3e4
Mark Ratjens <mark@ratjens.com>
2020-09-11 14:11:54 +1000

    remove superfluous code for generating docker steps

--- src/nodules/nodules.rb
+++ src/nodules/nodules.rb
@@ -3,16 +3,6 @@ require_relative '../releases/division'
 module Nodules
   class Nodules < ::Releases::Division
 
-    class << self
-      def step_precedence
-        { late: [:run] }
-      end
-
-      def inheritance_paths; __dir__ ;end
-    end
-
-    require_files_in :steps
-
     def subdivision_for(struct)
       universe.nodules.by(struct: struct, division: self)
     end
