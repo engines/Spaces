cf08a19f9a15ef8680c4cb171a4e6f8cd35dcc93
Mark Ratjens <mark@ratjens.com>
2020-09-18 13:47:16 +1000

    merge Collaboration into Release

--- src/releases/release.rb
+++ src/releases/release.rb
@@ -1,15 +1,52 @@
-require_relative 'collaboration'
+require_relative 'division'
 
 module Releases
-  class Release < Collaboration
+  class Release < Division
 
-    delegate(blueprints: :universe)
+    relation_accessor :predecessor
+
+    delegate(
+      blueprints: :universe,
+      identifier: :descriptor,
+      outline: :schema
+    )
 
     alias_method :has?, :respond_to?
 
     def count
       has?(:scaling) ? scaling.count : 1
     end
+
+    def context_identifier; identifier ;end
+
+    def memento; OpenStruct.new(to_h) ;end
+
+    def divisions; division_map.values ;end
+
+    def division_map
+      @division_map ||= keys.inject({}) do |m, k|
+        m.tap do
+          m[k] = division_for(k)
+        end
+      end.compact
+    end
+
+    def division_for(key)
+      schema.divisions[key]&.prototype(collaboration: self, label: key)
+    end
+
+    def to_h
+      division_keys.inject({}) do |m, k|
+        m.tap { m[k] = memento_for(k) }
+      end.compact
+    end
+
+    def memento_for(key)
+      division_map[key]&.memento || struct[key]
+    end
+
+    def schema_keys; schema.keys ;end
+    def division_keys; division_map.keys ;end
 
     def descriptors_for(division_identifier)
       descriptors_structs_for(division_identifier).map { |d| descriptor_class.new(d) }.uniq(&:uniqueness)
@@ -24,6 +61,18 @@ module Releases
     end
 
     def text_class; Texts::FileText ;end
+
+    def method_missing(m, *args, &block)
+      if division_keys.include?(m)
+        division_map[m.to_sym] || struct[m]
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(m, *)
+      division_keys.include?(m) || super
+    end
 
   end
 end
