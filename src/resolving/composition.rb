b95defadc4770a96d6d687f1038c182baee9579f
Mark Ratjens <mark@ratjens.com>
2020-09-22 14:27:52 +1000

    for now, remove resolving compositions auxiliary_divisions

--- src/resolving/composition.rb
+++ src/resolving/composition.rb
@@ -13,18 +13,17 @@ module Resolving
         ]
       end
 
-      def auxiliary_classes
-        [
-        ]
+      def associative_divisions
+        @associative_divisions ||= map_for(associative_classes)
       end
 
-      def associative_divisions; @associative_divisions ||= map_for(associative_classes) ;end
-      def auxiliary_divisions; @auxiliary_divisions ||= map_for(auxiliary_classes) ;end
+      def mandatory_keys
+        associative_divisions.keys
+      end
 
-      def mandatory_divisions; @mandatory_divisions ||= auxiliary_divisions.merge(associative_divisions) ;end
-      def mandatory_keys; mandatory_divisions.keys ;end
-
-      def divisions; mandatory_divisions.merge(super) ;end
+      def divisions
+        associative_divisions.merge(super)
+      end
 
     end
 
