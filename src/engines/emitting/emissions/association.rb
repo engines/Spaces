74d14f0f9cb8e88b4b1b5e513965b22ae191436a
Mark Ratjens <mark@ratjens.com>
2020-10-22 11:14:22 +1100

    FIX BUG: default division #prototype passes wrong params to associations#initialize
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- src/engines/emitting/emissions/association.rb
+++ src/engines/emitting/emissions/association.rb
@@ -1,9 +1,20 @@
 module Emissions
   class Association < ::Emissions::Division
 
+    class << self
+      def prototype(emission:, label:)
+        new(struct: struct_for(emission, label), label: label)
+      end
+
+      def struct_for(emission, label)
+        emission.struct[label] || default_struct
+      end
+    end
+
     alias_method :context_identifier, :identifier
 
-    def initialize(struct: nil, identifier: nil)
+    def initialize(struct: nil, identifier: nil, label: nil)
+      self.label = label
       self.struct = duplicate(struct) || OpenStruct.new
       self.struct.identifier = identifier if identifier
     end
