2c0564d94200df4407692837d86296c5f9573039
Mark Ratjens <mark@ratjens.com>
2020-10-26 11:24:47 +1100

    use resolution values in LXD::Image#default_resolution
    elimination tactical use of interpolation from poc
    
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- src/providers/virtualisation/lxd/image.rb
+++ src/providers/virtualisation/lxd/image.rb
@@ -4,11 +4,11 @@ module Providers
 
       def default_resolution
         @default_resolution ||= {
-          name: '{{user `suite`}}',
-          output_image: 'spaces/{{user `suite`}}/base/{{user `datestamp`}}{{user `tag`}}',
+          name: default_name,
+          output_image: default_output_image,
           publish_properties: {
-            description: 'Spaces {{user `suite`}} image',
-            aliases: '{{user `suite`}}',
+            description: "Spaces #{tenant.identifier} #{context_identifier} image",
+            aliases: tenant.identifier,
             architecture: 'amd64',
             os: 'devuan',
             release: 'Devuan GNU/Linux 3.0'
