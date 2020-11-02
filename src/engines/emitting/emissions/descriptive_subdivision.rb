2718a0adbad6403c401082461e56db3fdc888e2e
Lachlan Douglas <lachlan@engines.org>
2020-11-02 16:26:12 +1100

    Add blueprint method to DescriptiveSubdivision. Allows binding to find its targetted blueprint.

--- src/engines/emitting/emissions/descriptive_subdivision.rb
+++ src/engines/emitting/emissions/descriptive_subdivision.rb
@@ -9,6 +9,10 @@ module Emissions
       @resolution ||= universe.resolutions.by(identifier)
     end
 
+    def blueprint
+      @blueprint ||= universe.blueprints.by(descriptor)
+    end
+
     def descriptor; @descriptor ||= descriptor_class.new(struct.descriptor) ;end
     def identifier; struct.identifier || descriptor.identifier ;end
 
