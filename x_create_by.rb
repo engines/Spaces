6c8506a9ac5cfc5ea9f70c83ab4424fd126c9eb5
Lachlan Douglas <lachlan@engines.org>
2020-12-14 13:42:35 +1100

    Remove temporary files

--- x_create_by.rb
+++
@@ -1,9 +0,0 @@
-require_relative './src/universe'
-universe = Universe.new
-identifier = 'test_app'
-blueprint = Blueprinting::Blueprint.new(identifier: identifier)
-universe.blueprints.save(blueprint)
-universe.blueprints.by(identifier).to_json
-universe.resolutions.by(identifier).to_json
-universe.packing.by(identifier).to_json
-universe.provisioning.by(identifier).to_json
