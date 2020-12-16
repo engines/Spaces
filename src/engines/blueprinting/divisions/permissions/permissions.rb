fc57ebb28449554caac793e0185b578b30372a07
Mark Ratjens <mark@ratjens.com>
2020-12-16 13:27:24 +1100

    implement Permissions division

---
+++ src/engines/blueprinting/divisions/permissions/permissions.rb
@@ -0,0 +1,17 @@
+module Divisions
+  class Permissions < ::Emissions::Divisible
+
+    alias_method :divisible_embed, :embed
+
+    include ::Packing::Division
+
+    def embed(other); divisible_embed(other) ;end
+
+    def packing_stanza_for(key)
+      all.map(&:packing_stanza) if key == :late
+    end
+
+    def keys; [:late] ;end
+
+  end
+end
