11c084b1fd07a50eaa3058d303ed73f86cd429e7
Lachlan Douglas <lachlan@engines.org>
2020-12-21 13:42:02 +1100

    Temporary disable of blueprinting defaults.

--- api/helpers/system.rb
+++ api/helpers/system.rb
@@ -13,8 +13,8 @@ end
 # Data needed for certain options on blueprint forms.
 def blueprinting_settings
   {
-    build_scripts: Packing::Pack.script_file_names.map do |script|
-      {script => Packing::Pack.script_choices_names(script)}
-    end.inject(:merge)
+    # build_scripts: Packing::Pack.script_file_names.map do |script|
+    #   {script => Packing::Pack.script_choices_names(script)}
+    # end.inject(:merge)
   }
 end
