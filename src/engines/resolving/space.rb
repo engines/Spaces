297b66efb8b97a599033dc471f05e3a6966b26f9
Lachlan Douglas <lachlan@engines.org>
2021-01-14 06:09:14 +1100

    Resolve git conflict.

--- src/engines/resolving/space.rb
+++ src/engines/resolving/space.rb
@@ -24,9 +24,7 @@ module Resolving
 
     def save(model)
       super.tap do
-        model.auxiliary_content.each do |f|
-          save_text(model, f.content) 
-        end
+        model.auxiliary_content.each { |t| save_text(t) }
       end
     end
 
