bbb513208466094841be7489e41017cae3965c52
Lachlan Douglas <lachlan@engines.org>
2021-01-14 09:00:50 +1100

    Formatting

--- api/blueprints.rb
+++ api/blueprints.rb
@@ -1,4 +1,4 @@
 # List by recursive lookup of all blueprint dependencies.
 get '/blueprints/:identifier/turtles' do
-  ( universe.blueprints.by(params[:identifier]).turtle_descriptors || [] ).map(&:identifier).to_json
+  (universe.blueprints.by(params[:identifier]).turtle_descriptors || [] ).map(&:identifier).to_json
 end
