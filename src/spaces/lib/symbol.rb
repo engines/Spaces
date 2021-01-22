f08933191b71ea52c0e731883ea4dfd3c24c1942
Mark Ratjens <mark@ratjens.com>
2021-01-22 12:43:33 +1100

    define some helper methods for Symbol which delegate to String

---
+++ src/spaces/lib/symbol.rb
@@ -0,0 +1,11 @@
+class Symbol
+  BLANK_RE = /\A[[:space:]]*\z/
+
+  def camelize; to_s.camelize ;end
+  def snakize; to_s.snakize ;end
+  def singularize; to_s.singularize ;end
+  def pluralize; to_s.pluralize ;end
+  
+  alias_method :underscore, :snakize
+
+end
