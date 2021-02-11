1413f8c1f19f7f8b7e67abd8c57c1c0f8d23b82c
James Vodanovich <james@jvodan.com>
2021-02-11 14:35:02 +1100

    rb

--- src/engines/emissions/transformable.rb
+++ src/engines/emissions/transformable.rb
@@ -9,7 +9,7 @@ module Emissions
 
     def descriptor_class; ::Spaces::Descriptor ;end
     def arena_stanzas; stanzas_for(:arena) ;end
-    def providers_required; stanzas_for(:arena) ;end #?
+ 
     def provisioning_stanzas; stanzas_for(:provisioning) ;end
 
     def stanzas_for(symbol); _stanzas_for(symbol) ;end
