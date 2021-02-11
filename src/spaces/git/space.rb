73a85556801f592836fb501c193a5fe19e5ae8df
Mark Ratjens <mark@ratjens.com>
2021-02-11 15:58:32 +1100

    allow for importing bindings from alternative accounts and branches

--- src/spaces/git/space.rb
+++ src/spaces/git/space.rb
@@ -10,11 +10,25 @@ module Git
     def import(descriptor)
       ensure_space
       begin
-        g = ::Git.clone(descriptor.repository, descriptor.identifier, path: path)
-        g.checkout(descriptor.branch) if descriptor.branch
+        g = ::Git.clone(repository(descriptor), descriptor.identifier, path: path)
+        g.checkout(branch(descriptor)) if branch(descriptor)
       rescue ::Git::GitExecuteError => e
         warn(error: e, descriptor: descriptor, verbosity: [:error])
       end
+    end
+
+    def repository(descriptor)
+      fork_account ? "#{descriptor.repository}".gsub(default_account, fork_account): descriptor.repository
+    end
+
+    def branch(descriptor)
+      descriptor.branch
+    end
+
+    def default_account; 'v2Blueprints' ;end
+    def fork_account
+      # 'MarkRatjens'
+      default_account
     end
 
   end
--- src/x/publishing.rb
+++ src/x/publishing.rb
@@ -1,3 +1,5 @@
+def descriptor; @descriptor ;end
+
 def import; universe.publications.import(descriptor, force: true); end
 
 def publication
