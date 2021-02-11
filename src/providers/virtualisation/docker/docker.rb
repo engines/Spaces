73b60e51e8987928e0cf458d9dfb2a8d25eda3c5
James Vodanovich <james@jvodan.com>
2021-02-11 12:08:02 +1100

    add provider_require

--- src/providers/virtualisation/docker/docker.rb
+++ src/providers/virtualisation/docker/docker.rb
@@ -1,12 +1,12 @@
 module Providers
   class Docker < ::Divisions::Provider
-
     def arena_stanzas
-      %Q(
+      %(
         provider "#{type}" {
         }
       )
     end
 
+    def providers_require; end
   end
 end
