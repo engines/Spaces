7d080734c08583b07142da63191232df5573a316
Mark Ratjens <mark@ratjens.com>
2021-02-22 16:54:48 +1100

    implement #prototype for Divisions::Provider

--- src/engines/blueprinting/divisions/provider.rb
+++ src/engines/blueprinting/divisions/provider.rb
@@ -1,8 +1,22 @@
 module Divisions
   class Provider < ::Divisions::Division
 
-    def implementation
-      struct.implementation || context_identifier
+    class << self
+      def prototype(emission:, label:)
+        constant_for(struct_for(emission).type).new(emission: emission, label: label)
+      end
+
+      def constant_for(type)
+        Module.const_get("::Providers::#{type.to_s.camelize}")
+      end
+
+      def struct_for(emission)
+        emission.struct.provider
+      end
+    end
+
+    def type
+      struct.type || context_identifier
     end
 
   end
