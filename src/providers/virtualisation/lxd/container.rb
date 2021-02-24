014bc8861cedca1c1583431a44b6edc782806e6a
Mark Ratjens <mark@ratjens.com>
2021-02-24 14:29:37 +1100

    add provisioner stanza to blueprint_stanzas
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- src/providers/virtualisation/lxd/container.rb
+++ src/providers/virtualisation/lxd/container.rb
@@ -9,11 +9,14 @@ module Providers
       end
 
       def container_stanza(i)
-        %Q(
-          resource "lxd_container" "#{blueprint_identifier}-#{i+1}" {
-            name      = "#{blueprint_identifier}-#{i+1}"
+        ci = container_identifier_for(i+1)
+
+        %(
+          resource "lxd_container" "#{ci}" {
+            name      = "#{ci}"
             image     = "#{image_name}"
             ephemeral = false
+            profiles = ["default"]
 
             device {
               name  = "root"
@@ -25,7 +28,11 @@ module Providers
               }
             }
 
-              #{device_stanzas}
+            provisioner "local-exec" {
+              command = "lxc exec #{ci} /root/setup.sh"
+            }
+
+            #{device_stanzas}
 
             config = {
               "boot.autostart" = true
@@ -36,6 +43,10 @@ module Providers
 
       def device_stanzas
         emission.volumes.all.map(&:device_stanzas).join("\n") if emission.has?(:volumes)
+      end
+
+      def container_identifier_for(subscript)
+        "#{blueprint_identifier}-#{subscript}"
       end
 
     end
