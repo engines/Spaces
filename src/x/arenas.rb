7ea333a420a5e888972baefdc6154ea4b09e5524
Mark Ratjens <mark@ratjens.com>
2021-02-25 16:59:37 +1100

    put test scripts in one file
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- src/x/arenas.rb
+++
@@ -1,7 +0,0 @@
-def arena; @arena ||= universe.arenas.by('development') ;end
-
-def save_arena; universe.arenas.save(arena) ;end
-
-def init_arena; universe.arenas.init(arena) ;end
-def plan_arena; universe.arenas.plan(arena) ;end
-def apply_arena; universe.arenas.apply(arena) ;end
--- src/x/blueprints.rb
+++
@@ -1,8 +0,0 @@
-def blueprint
-  @blueprint ||= universe.blueprints.by(descriptor.identifier)
-end
-
-def with_embeds; blueprint.with_embeds ;end
-
-
-def save_blueprint; universe.blueprints.save(blueprint) ;end
