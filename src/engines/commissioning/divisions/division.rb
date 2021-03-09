e8b11a0505902b8f7f0c0171daf2d0df7ce14eb5
Mark Ratjens <mark@ratjens.com>
2021-03-09 15:58:43 +1100

    only commissioning scripts called ‘setup.sh’ will be executed
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- src/engines/commissioning/divisions/division.rb
+++ src/engines/commissioning/divisions/division.rb
@@ -4,7 +4,7 @@ module Commissioning
     def precedence; [:first, :early, :adds, :middle, :late, :removes, :last] ;end
 
     def commissioning_scripts
-      precedence.map { |p| paths_for(p) }.flatten.compact
+      precedence.map { |p| paths_for(p) }.flatten.compact.select { |p| p.basename == Pathname('setup.sh')}
     end
 
     def paths_for(precedence)
@@ -14,16 +14,6 @@ module Commissioning
     def source_paths_for(precedence)
       resolutions.file_names_for("#{:commissioning}/#{precedence}", context_identifier)
     end
-
-    # def uses?(precedence); keys.include?(precedence.to_sym) ;end
-    #
-    # def keys; by_precedence(super) ;end
-    #
-    # def by_precedence(keys)
-    #   keys.sort_by { |k| precedence.index(k) || precedence_midpoint }
-    # end
-
-    # def precedence_midpoint; precedence.count / 2 ;end
 
   end
 end
