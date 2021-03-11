923b6f59bf14b726c1bc367d3c6aacfa7f653ef4
Mark Ratjens <mark@ratjens.com>
2021-03-11 19:55:03 +1100

    REFACTOR: simplify processing of important divisions
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- src/engines/transforming/emissions/targeting.rb
+++ src/engines/transforming/emissions/targeting.rb
@@ -1,10 +1,6 @@
 module Emissions
   module Targeting
 
-    def bindings
-      has?(:bindings) ? division_map[:bindings] : division_for(:bindings)
-    end
-
     def turtles; turtle_targets.map(&:blueprint) ;end
     def turtle_targets; targets(:turtle_targets) ;end
 
