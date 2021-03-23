85bab86a1f7ce017119c15e4a013aee996db6d80
Mark Ratjens <mark@ratjens.com>
2021-03-23 16:55:52 +1100

    define Space#payload_path_for(model)
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/engines/arenas/space.rb
+++ lib/spaces/engines/arenas/space.rb
@@ -9,12 +9,18 @@ module Arenas
 
     def save(model)
       super.tap do
-        _save(model, content: model.stanzas_content, as: :tf)
+        _save(model, content: model.stanzas_content, as: payload_extension)
       end
+    end
+
+    def payload_path_for(model)
+      path_for(model).join("*.#{payload_extension}")
     end
 
     def path_for(model)
       path.join(model.arena.context_identifier)
     end
+
+    def payload_extension; :tf ;end
   end
 end
