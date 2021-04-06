eb17330c737fced5dbad34127e332fa5c8f588c2
Mark Ratjens <mark@ratjens.com>
2021-04-06 14:09:56 +1000

    implement #deflated for volume divisions
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/engines/blueprinting/divisions/volumes/volume.rb
+++ lib/spaces/engines/blueprinting/divisions/volumes/volume.rb
@@ -1,6 +1,10 @@
 module Divisions
   class Volume < ::Divisions::Subdivision
 
+    class << self
+      def features; [:source, :destination] ;end
+    end
+
     alias_method :identifier, :context_identifier
 
     def device_stanzas; ;end
