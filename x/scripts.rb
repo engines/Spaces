17dd0b2f3ec94d94b35af74b294c690210f86c14
Mark Ratjens <mark@ratjens.com>
2021-04-27 12:52:20 +1000

    update scripts to use #payload
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- x/scripts.rb
+++ x/scripts.rb
@@ -10,73 +10,73 @@ require 'spaces'
 universe = Universe.universe
 
 # delete an arena
-Spaces::Commands::Deleting.new(identifier: :development, space: :arenas).run.struct
+Spaces::Commands::Deleting.new(identifier: :development, space: :arenas).run.payload
 
 # save a basic arena with default associations
-Arenas::Commands::Saving.new(identifier: :development).run.struct
+Arenas::Commands::Saving.new(identifier: :development).run.payload
 
 # import an arena bootstrap
 Publishing::Commands::Importing.new(
   model: {repository: 'https://github.com/v2Blueprints/arena'},
   force: true
-).run.struct
+).run.payload
 
 # bootstrap the arena
-Bootstrapping::Commands::Initializing.new(identifier: :development, blueprint_identifier: 'arena').run.struct
+Bootstrapping::Commands::Initializing.new(identifier: :development, blueprint_identifier: 'arena').run.payload
 
 # resolve the bootstrap
-Bootstrapping::Commands::Resolving.new(identifier: :development).run.struct
+Bootstrapping::Commands::Resolving.new(identifier: :development).run.payload
 
 # save all packs for an arena
-Arenas::Commands::Packing.new(identifier: :development).run.struct
+Arenas::Commands::Packing.new(identifier: :development).run.payload
 
 # provision the bootstrap
-Arenas::Commands::Provisioning.new(identifier: :development).run.struct
+Arenas::Commands::Provisioning.new(identifier: :development).run.payload
 
 # import a blueprint
 Publishing::Commands::Importing.new(
   model: {repository: 'https://github.com/v2Blueprints/phpmyadmin'},
   force: true
-).run.struct
+).run.payload
 
 # get the status of a publication
-Spaces::Commands::Status.new(identifier: :phpmyadmin, space: :publications).run.struct
+Spaces::Commands::Status.new(identifier: :phpmyadmin, space: :publications).run.payload
 
 # synchronize a blueprint
-Blueprinting::Commands::Synchronizing.new(identifier: :phpmyadmin).run.struct
+Blueprinting::Commands::Synchronizing.new(identifier: :phpmyadmin).run.payload
 
 # synchronize a publication
-Publishing::Commands::Synchronizing.new(identifier: :phpmyadmin).run.struct
+Publishing::Commands::Synchronizing.new(identifier: :phpmyadmin).run.payload
 
 # get the topology for a blueprint
-Spaces::Commands::Graphing.new(identifier: :phpmyadmin, space: :blueprints).run.struct
+Spaces::Commands::Graphing.new(identifier: :phpmyadmin, space: :blueprints).run.payload
 
 # resolve a blueprint
-Blueprinting::Commands::Resolving.new(identifier: :phpmyadmin, arena_identifier: :development).run.struct
+Blueprinting::Commands::Resolving.new(identifier: :phpmyadmin, arena_identifier: :development).run.payload
 
 # validate a resolution
-Spaces::Commands::Validating.new(identifier: 'development::phpmyadmin', space: :resolutions).run.struct
+Spaces::Commands::Validating.new(identifier: 'development::phpmyadmin', space: :resolutions).run.payload
 
 # get the status of a resolution
-Spaces::Commands::Status.new(identifier: 'development::phpmyadmin', space: :resolutions).run.struct
+Spaces::Commands::Status.new(identifier: 'development::phpmyadmin', space: :resolutions).run.payload
 
 # get the identifiers of resolutions in an arena
-Spaces::Commands::Querying.new(method: :identifiers, arena_identifier: :development, space: :resolutions).run.struct
+Spaces::Commands::Querying.new(method: :identifiers, arena_identifier: :development, space: :resolutions).run.payload
 
 # save a pack for a resolution
-Packing::Commands::Saving.new(identifier: 'development::phpmyadmin').run.struct
+Packing::Commands::Saving.new(identifier: 'development::phpmyadmin').run.payload
 
 # get the identifiers of packs in an arena
-Spaces::Commands::Querying.new(method: :identifiers, arena_identifier: :development, space: :packs).run.struct
+Spaces::Commands::Querying.new(method: :identifiers, arena_identifier: :development, space: :packs).run.payload
 
 # get the artifacts for a pack
-Packing::Commands::Artifacts.new(identifier: 'development::phpmyadmin').run.struct
+Packing::Commands::Artifacts.new(identifier: 'development::phpmyadmin').run.payload
 
 # save provisions for resolution
-Provisioning::Commands::Saving.new(identifier: 'development::phpmyadmin').run.struct
+Provisioning::Commands::Saving.new(identifier: 'development::phpmyadmin').run.payload
 
 # save all provisions for an arena
-Arenas::Commands::Provisioning.new(identifier: :development).run.struct
+Arenas::Commands::Provisioning.new(identifier: :development).run.payload
 
 # params = {
 #   identifier: 'lxd',
