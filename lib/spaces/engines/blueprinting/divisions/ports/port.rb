523a34bd4b8f96e54373d5c7f833a79e2b60e141
Mark Ratjens <mark@ratjens.com>
2021-05-11 17:30:01 +1000

    FIX SYNTAX ERROR: Port#features missing comma in array
    Must never have been tested!
    
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/engines/blueprinting/divisions/ports/port.rb
+++ lib/spaces/engines/blueprinting/divisions/ports/port.rb
@@ -2,7 +2,7 @@ module Divisions
   class Port < ::Divisions::Subdivision
 
     class << self
-      def features; [:start_port, :end_port :protocol, :external_port] ;end
+      def features; [:start_port, :end_port, :protocol, :external_port] ;end
     end
 
   end
