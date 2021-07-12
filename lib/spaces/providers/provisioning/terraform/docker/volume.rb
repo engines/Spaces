f87bcb26c451f2fe29eaa477ee934ac826b983ea
James Vodanovich <james@jvodan.com>
2021-07-12 13:16:38 +1000

    volumes and fix image_host

--- lib/spaces/providers/provisioning/terraform/docker/volume.rb
+++ lib/spaces/providers/provisioning/terraform/docker/volume.rb
@@ -1,7 +1,15 @@
 module Providers
   class Docker < ::ProviderAspects::Provider
     class Volume < ::ProviderAspects::Volume
-
+      def device_stanzas
+        %Q(
+          resource "docker_volume" "#{volume_name}"  {
+            name = "#{volume_name}"
+            driver = "local"           
+            mountpoint "#{destination}"
+          }
+        )
+      end
     end
   end
 end
