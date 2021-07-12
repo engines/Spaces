6b829a8138609fcdaa6a9564b53e31318449ddd1
James Vodanovich <james@jvodan.com>
2021-07-13 09:47:04 +1000

    docker volume methods

--- lib/spaces/providers/provisioning/terraform/docker/volume.rb
+++ lib/spaces/providers/provisioning/terraform/docker/volume.rb
@@ -1,12 +1,21 @@
 module Providers
   class Docker < ::ProviderAspects::Provider
     class Volume < ::ProviderAspects::Volume
+
+      def resolution_stanzas_for(_)
+        %Q(
+          volumes { 
+            volume_name = "#{volume_name}"
+			 container_path = "#{destination}"
+          }
+        )
+      end
+
       def device_stanzas
         %Q(
           resource "docker_volume" "#{volume_name}"  {
             name = "#{volume_name}"
             driver = "local"           
-            mountpoint "#{destination}"
           }
         )
       end
