8b13b703635ea238ee4a5573b962b32c8745cee6
Mark Ratjens <mark@ratjens.com>
2021-07-13 14:20:28 +1000

    FIX: duplicate implementation of #spaces_image_registry
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/provisioning/terraform/docker/container.rb
+++ lib/spaces/providers/provisioning/terraform/docker/container.rb
@@ -1,6 +1,7 @@
 module Providers
   class Docker < ::ProviderAspects::Provider
     class Container < ::ProviderAspects::Container
+
       def resolution_stanzas_for(_)
         %(
           resource "#{container_type}" "#{blueprint_identifier}" {
@@ -15,14 +16,10 @@ module Providers
         # "#{image_host}:" if image_host
       end
 
-
       def device_stanzas
         provisions.volumes.all.map(&:device_stanzas).join
       end
 
-      def spaces_image_registry
-      #  image_host.nil? ? nil : "#{image_host}:"
-      end
     end
   end
 end
