00a530637289022871ed280068aa26ac62db9a9f
James Vodanovich <james@jvodan.com>
2021-07-14 14:16:17 +1000

    add dependancies

--- lib/spaces/providers/provisioning/terraform/docker/container.rb
+++ lib/spaces/providers/provisioning/terraform/docker/container.rb
@@ -1,7 +1,6 @@
 module Providers
   class Docker < ::ProviderAspects::Provider
     class Container < ::ProviderAspects::Container
-
       def resolution_stanzas_for(_)
         %(
           resource "#{container_type}" "#{blueprint_identifier}" {
@@ -14,10 +13,13 @@ module Providers
         )
       end
 
-  	  def spaces_image_registry
+      def spaces_image_registry
         # "#{image_host}:" if image_host
       end
 
+      def dependency_string
+        connections_down.map { |c| "#{container_type}.#{c.blueprint_identifier}" }.join(', ')
+      end
     end
   end
 end
