12648766a34729e7645f3c1e7b0a194c753c7a86
Mark Ratjens <mark@ratjens.com>
2021-07-15 16:56:04 +1000

    gah!
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/provisioning/terraform/docker/container.rb
+++ lib/spaces/providers/provisioning/terraform/docker/container.rb
@@ -1,6 +1,7 @@
 module Providers
   class Docker < ::ProviderAspects::Provider
     class Container < ::ProviderAspects::Container
+
       def resolution_stanzas_for(_)
         %(
           resource "#{container_type}" "#{blueprint_identifier}" {
