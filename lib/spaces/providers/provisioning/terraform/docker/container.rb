fb2916572635afd9e82700cb3f39d5edccba1079
Mark Ratjens <mark@ratjens.com>
2021-07-15 11:50:32 +1000

    minor linting to DockerContainer
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/provisioning/terraform/docker/container.rb
+++ lib/spaces/providers/provisioning/terraform/docker/container.rb
@@ -5,16 +5,24 @@ module Providers
       def resolution_stanzas_for(_)
         %(
           resource "#{container_type}" "#{blueprint_identifier}" {
-            name      = "#{blueprint_identifier}"
-            image     = "#{spaces_image_registry}#{image_name}"
+            name = "#{blueprint_identifier}"
+            image = "#{spaces_image_registry}#{image_name}"
+      			domainname = "#{universe.host}"
+      			hostname = "#{blueprint_identifier}"
+
+            #{connect_services_stanzas}
+            #{device_stanzas}
           }
         )
       end
 
-  	  def spaces_image_registry
+      def spaces_image_registry
         # "#{image_host}:" if image_host
       end
 
+      def dependency_string
+        connections_down.map { |c| "#{container_type}.#{c.blueprint_identifier}" }.join(', ')
+      end
     end
   end
 end
