2b0bf6f9709bc1dd6b30d0a32263bb24f07c2ef2
Mark Ratjens <mark@ratjens.com>
2021-08-11 17:25:44 +1000

    define a copying command
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

---
+++ lib/spaces/spaces/commands/copying.rb
@@ -0,0 +1,25 @@
+require_relative 'saving'
+
+module Spaces
+  module Commands
+    class Copying < Saving
+
+      alias_method :model, :current_model
+
+      def assembly
+        model.tap { model.identifier = new_identifier }
+      end
+
+      def new_identifier
+        (input[:new_identifier] || [identifier,'_copy'].join).to_s
+      end
+
+      protected
+
+      def commit
+        space.save(assembly)
+      end
+
+    end
+  end
+end
