86b57a6290a191155ce2750133840987aaeb9a7c
Mark Ratjens <mark@ratjens.com>
2021-08-18 18:05:46 +1000

    FIX typo
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/spaces/git/space.rb
+++ lib/spaces/spaces/git/space.rb
@@ -17,7 +17,7 @@ module Spaces
       end
 
       def location_maybe_already_set_for(descriptor)
-        locations.exist_then_by(descriptor) || ddescriptor
+        locations.exist_then_by(descriptor) || descriptor
       end
 
       def repository_class; ::Spaces::Git::Repository ;end
