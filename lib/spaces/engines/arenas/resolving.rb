44b885fbb55cda3fd4d7d56e7be6210a52fda809
Lachlan Douglas <lachlan@engines.org>
2021-08-25 11:33:11 +1000

    Write builder output to log file. Pass blocks from controller through to build commit method.

--- lib/spaces/engines/arenas/resolving.rb
+++ lib/spaces/engines/arenas/resolving.rb
@@ -7,7 +7,7 @@ module Arenas
     def resolved; present_in(resolutions) ;end
     def unresolved; absent_in(resolutions) ;end
 
-    def unsaved_resolutions; unsaved(:resolution) ;end
+    def unsaved_resolutions; unsaved(:resolutions) ;end
     def bound_resolutions; resolution_map.values ;end
 
     def resolution_map; bound_map_for(:resolution) ;end
