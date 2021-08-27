8a1895adf581ae23d6bb4b4102fdcce8c26e6a10
Mark Ratjens <mark@ratjens.com>
2021-08-27 12:34:11 +1000

    FIX BUG: Keys::Space#identifiers returns rubbish for keys without a tie_breaker
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/engines/keys/space.rb
+++ lib/spaces/engines/keys/space.rb
@@ -7,8 +7,10 @@ module Keys
       end
     end
 
-    def identifiers(domain: '*', username: '*', tie_breaker: nil)
+    def identifiers(domain: '*', username: '*', tie_breaker: '*')
       path.glob([domain, username, tie_breaker].compact.join('/')).map do |p|
+        p.directory? ? p : p.parent
+      end.uniq.map do |p|
         "#{p.relative_path_from(path)}".as_compound
       end
     end
