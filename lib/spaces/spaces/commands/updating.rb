11758c34310e4c91a6c909ec9d97fdba332b9122
Mark Ratjens <mark@ratjens.com>
2021-09-10 12:55:57 +1000

    define Creating & Updating commands and incorporate into a keys controller
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

---
+++ lib/spaces/spaces/commands/updating.rb
@@ -0,0 +1,13 @@
+require_relative 'saving'
+
+module Spaces
+  module Commands
+    class Updating < Saving
+
+      def model_struct
+        current_model.struct.merge(super)
+      end
+
+    end
+  end
+end
