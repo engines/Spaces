b74179468fc5f5a1f86cac4ae580863a7e60c92e
Lachlan Douglas <lachlan@engines.org>
2021-09-13 19:45:13 +1000

    fix require

--- lib/gems/git.rb
+++ lib/gems/git.rb
@@ -6,6 +6,7 @@
 #   - &block is passed to git command to capture output from IO
 # - add --verbose and --progress options to yield more output
 require 'git'
+require 'gems/git/git'
 require 'gems/git/git/base'
 require 'gems/git/git/branch'
 require 'gems/git/git/lib'
