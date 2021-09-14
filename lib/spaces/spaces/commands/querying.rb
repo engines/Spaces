engines/Spaces

commit 83d4694e64417907169528bfff5b1b425b0568f4

typo

lib/spaces/spaces/commands/querying.rb
@@ -4,11 +4,12 @@ module Spaces
   module Commands
     class Querying < Command
 
-      def methud_signature
-        [methud, arguments].compact
+      def method_signature
+        # debugger
+        [method, arguments].compact
       end
 
-      def methud
+      def method
         input[:method] || (raise ::Spaces::Errors::MissingInput, {input: input})
       end
 
@@ -17,7 +18,8 @@ module Spaces
       end
 
       def models
-        @models ||= space.send(*methud_signature)
+        # debugger
+        @models ||= space.send(*method_signature)
       end
 
       alias_method :assembly, :models
@@ -25,7 +27,8 @@ module Spaces
       protected
 
       def _arguments
-        input.slice(space.method(methud).parameters.map(&:last))
+        # debugger
+        input.slice(space.method(method).parameters.map(&:last))
       end
 
     end

commit d6d250fd7218ed94953841737c4e2b14a1a30f1e

FIX REGRESSION
83d4694e64417907169528bfff5b1b425b0568f4

broke

f68623392cab97cab67cc731fbdf84ffc965f3ec

Signed-off-by: Mark Ratjens <mark@ratjens.com>

lib/spaces/spaces/commands/querying.rb
@@ -5,10 +5,10 @@ module Spaces
     class Querying < Command
 
       def method_signature
-        [method, arguments].compact
+        [query_method, arguments].compact
       end
 
-      def method
+      def query_method
         input[:method] || (raise ::Spaces::Errors::MissingInput, {input: input})
       end
 
@@ -25,7 +25,7 @@ module Spaces
       protected
 
       def _arguments
-        input.slice(space.method(method).parameters.map(&:last))
+        input.slice(space.method(query_method).parameters.map(&:last))
       end
 
     end

x/scripts_docker.rb
@@ -108,7 +108,7 @@ controllers.packing.create(identifier: 'docker_arena::phpmyadmin')
 controllers.arenas.state(identifier: :docker_arena)
 
 # # get the identifiers of packs in an arena
-# controllers.querying.list((method: :identifiers, arena_identifier: :docker_arena, space: :packs)
+# controllers.querying.list(method: :identifiers, arena_identifier: :docker_arena, space: :packs)
 
 # # get the artifacts for a pack
 # controllers.packing.artifacts(identifier: 'docker_arena::phpmyadmin')