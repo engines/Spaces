abecb8ddc692ee8fbfc7f3293abf5b05010e2085
Mark Ratjens <mark@ratjens.com>
2021-09-14 18:30:48 +1000

    REFACTOR: extract a Control class from Controller
    ... which represents a Command class and a specific set of arguments for creating an instance of the command
    
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/spaces/controllers/controller.rb
+++ lib/spaces/spaces/controllers/controller.rb
@@ -3,64 +3,60 @@ module Spaces
     class Controller < ::Spaces::Model
 
       def control(args, &block)
-        log_control(args, &block)
-        args[:threaded] ? _control_with_threading(args, &block) : _control(args, &block)
+        instance_for(args, &block).attempt
       end
 
-      def command_for(args)
-        action_class_for(args[:action]).new(**command_args_for(args))
+      def instance_for(args, &block)
+        instance_class.new(signature_for(args), &block)
       end
 
-      def command_args_for(args)
+      def instance_class; Control ;end
+
+      def signature_for(**args)
+        OpenStruct.new(
+          klass: class_for(args[:action]),
+          arguments: full_args_for(**args)
+        )
+      end
+
+      def full_args_for(**args)
         default_args
-        .merge(action_args_for(args[:action]))
+        .merge(args_for(args[:action]))
         .merge(args.without(:action))
       end
 
-      def default_args; struct.to_h ;end
-
-      def action_class_for(action)
-        action_array_for(action).first
+      def args_for(action)
+        array_for(action)[1] || {}
       end
 
-      def action_args_for(action)
-        action_array_for(action)[1] || {}
+      def class_for(action)
+        array_for(action).first
       end
 
-      def action_array_for(action)
+      def array_for(action)
         [action_command_map[action.to_sym]].flatten
       end
 
       def action_command_map; {} ;end
 
+      def default_args; struct.to_h ;end
+
       def initialize(**args)
-        self.struct = OpenStruct.new(args.symbolize_keys)
-      end
-
-      def method_missing(m, **args, &block)
-        control(action: m, **args, &block) if action_command_map.keys.include?(m)
-      end
-
-      def respond_to_missing?(m, *)
-        action_command_map.keys.include?(m)
-      end
-
-      protected
-
-      def _control_with_threading(args, &block)
-        (Thread.new { _control(args, &block) }).join
-      end
-
-      def _control(with: [:run, :payload], **args, &block)
-        with.reduce(command_for(args)) { |c, w| c.send(w, &block) }
+        self.struct = struct_in_space(**args)
       end
 
       def struct_in_space(**args)
         OpenStruct.new({space: space_identifier}.merge(args.symbolize_keys))
       end
 
-      def log_control(args, &block)
-        logger.info("Controller command: #{args} #{block_given? ? block : 'Block not given'}")
+      def space_identifier; ;end
+
+      def method_missing(m, **args, &block)
+        (control(action: m, **args, &block) if action_command_map.keys.include?(m))
+      end
+
+      def respond_to_missing?(m, *)
+        action_command_map.keys.include?(m)
       end
 
     end
