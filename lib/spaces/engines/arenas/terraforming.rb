8f74a987bfba7c3b1a22882f99c0e287cb87f4ed
Mark Ratjens <mark@ratjens.com>
2021-09-28 14:58:18 +1000

    derive arena artifiact from a provider aspect
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/engines/arenas/terraforming.rb
+++ lib/spaces/engines/arenas/terraforming.rb
@@ -2,69 +2,13 @@ module Arenas
   module Terraforming
     include ::Spaces::Streaming
 
-    def init(model); execute(:init, model) ;end
-    def plan(model); execute(:plan, model) ;end
-    def show(model); execute(:show, model) ;end
-    def apply(model); execute(:apply, model) ;end
+    def init(model); execute_on_aspect(:init, model) ;end
+    def plan(model); execute_on_aspect(:plan, model) ;end
+    def show(model); execute_on_aspect(:show, model) ;end
+    def apply(model); execute_on_aspect(:apply, model) ;end
 
-    protected
-
-    def execute(command, model)
-      with_streaming(model, command) do
-        identifier.tap { provisioning_for(command, model) }
-      end
-    end
-
-    def provisioning_for(command, model)
-      Dir.chdir(path_for(model)) do
-        bridge.send(command, options[command] || {}, config(stream_for(model, command)))
-      rescue RubyTerraform::Errors::ExecutionError => e
-        raise e
-      end
-    end
-
-    def config(stream)
-      {
-        stdout: stdout(stream),
-        stderr: stdout(stream),
-        logger: logger
-      }
-    end
-
-    def stdout(stream)
-      ->(output) do
-        stream.append(output_json_for(output))
-        logger.info(output)
-      end
-    end
-
-    def stderr(stream)
-      ->(error) do
-        stream.append(error_json_for(error))
-        logger.warn(error)
-      end
-    end
-
-    def output_json_for(output)
-      {output: output}.to_json
-    end
-
-    def error_json_for(error)
-      {error: error}.to_json
-    end
-
-    def bridge; RubyTerraform ;end
-
-    def options
-      {
-        plan: {
-          input: false
-        },
-        apply: {
-          input: false,
-          auto_approve: true
-        }
-      }
+    def execute_on_aspect(command, model)
+      provider_aspect_for(model, space).execute(command, model)
     end
 
   end
