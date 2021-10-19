86fd8c67151e68b6b35f4b1d32d805f76708a6b9
Mark Ratjens <mark@ratjens.com>
2021-10-19 11:32:45 +1100

    tidy up docker test script and include more arena bindings
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- x/scripts_docker.rb
+++ x/scripts_docker.rb
@@ -6,12 +6,8 @@
 
 # ------------------------------------------------------------------------------
 
-# import a bootstrappy blueprint
-# controllers.publishing.import(model: {repository: 'https://github.com/v2Blueprints/docker_arena'}, threaded: false)
-
-# ------------------------------------------------------------------------------
-
-# import an application blueprint
+# import some blueprints
+controllers.publishing.import(model: {repository: 'https://github.com/v2Blueprints/docker_arena'}, threaded: false)
 controllers.publishing.import(model: {repository: 'https://github.com/v2Blueprints/phpmyadmin'}, threaded: false)
 
 # ------------------------------------------------------------------------------
@@ -38,48 +34,6 @@ controllers.arenas.more_binders(identifier: :docker_arena)
 
 # ------------------------------------------------------------------------------
 
-# bind a bootstrappy blueprint to the arena
-# controllers.arenas.bind(identifier: :docker_arena, blueprint_identifier: :docker_arena)
-# controllers.arenas.state(identifier: :docker_arena)
-
-# ------------------------------------------------------------------------------
-
-# save installations for the arena so far
-# controllers.arenas.install(identifier: :docker_arena) #-----------------------
-# controllers.arenas.state(identifier: :docker_arena) #-------------------------
-
-# resolve the arena so far
-# controllers.arenas.resolve(identifier: :docker_arena)
-# controllers.arenas.state(identifier: :docker_arena)
-
-# ------------------------------------------------------------------------------
-
-# save all packs for an arena
-# controllers.arenas.pack(identifier: :docker_arena)
-# controllers.arenas.state(identifier: :docker_arena)
-# RUN PACKER HERE?
-
-# save provisions for the arena's runtime
-# controllers.arenas.runtime(identifier: :docker_arena)
-# controllers.arenas.state(identifier: :docker_arena)
-# RUN INIT HERE?
-
-# save provisions for the arena's other providers
-# controllers.arenas.provision(identifier: :docker_arena)
-# controllers.arenas.state(identifier: :docker_arena)
-# RUN APPLY HERE FOR INITIAL PROVISIONING? IT MUST HAPPEN BEFORE ...
-
-# save post-initialization provisions for providers
-# controllers.arenas.provision_prerequisites(identifier: :docker_arena)
-# controllers.arenas.state(identifier: :docker_arena)
-# RUN APPLY HERE FOR INITIAL PROVISIONING?
-
-#
-# THE ARENA SHOULD BE BOOSTRAPPED BY THIS POINT
-#
-
-# ------------------------------------------------------------------------------
-
 # synchronize a blueprint to a publication
 controllers.blueprinting.synchronize(identifier: :phpmyadmin)
 
@@ -88,7 +42,8 @@ controllers.publishing.synchronize(identifier: :phpmyadmin)
 
 # ------------------------------------------------------------------------------
 
-# bind another blueprint to the arena
+# bind some blueprints to the arena
+controllers.arenas.bind(identifier: :docker_arena, blueprint_identifier: :docker_arena)
 controllers.arenas.bind(identifier: :docker_arena, blueprint_identifier: :phpmyadmin)
 controllers.arenas.state(identifier: :docker_arena)
 
@@ -104,17 +59,18 @@ controllers.arenas.state(identifier: :docker_arena)
 # Spaces::Commands::Validating.create(identifier: 'docker_arena::phpmyadmin', space: :resolutions).run.payload
 
 # save all packs for an arena
-# controllers.arenas.pack(identifier: :docker_arena)
+controllers.arenas.pack(identifier: :docker_arena)
+controllers.arenas.state(identifier: :docker_arena)
 
 # save a pack for a resolution
-controllers.packing.create(identifier: 'docker_arena::phpmyadmin')
-controllers.arenas.state(identifier: :docker_arena)
+# controllers.packing.create(identifier: 'docker_arena::phpmyadmin')
+# controllers.arenas.state(identifier: :docker_arena)
 
 # # get the identifiers of packs in an arena
 # controllers.querying.list(method: :identifiers, arena_identifier: :docker_arena, space: :packs)
 
-# # get the artifacts for a pack
-# controllers.packing.artifacts(identifier: 'docker_arena::phpmyadmin')
+# # get the artifact for a pack
+# controllers.packing.artifact(identifier: 'docker_arena::phpmyadmin')
 
 # provision the arena for applications
 controllers.arenas.provision(identifier: :docker_arena)
