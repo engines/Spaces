a0db7d9e1104d92827c114ebcf959bc7873420bb
Mark Ratjens <mark@ratjens.com>
2021-10-21 12:00:56 +1100

    FIX REGRESSION: resolutions include image divisions for spurious runtimes
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/models/divisions/images/images.rb
+++ lib/spaces/models/divisions/images/images.rb
@@ -1,6 +1,10 @@
 module Divisions
   class Images < ::Divisions::Divisible
 
+    def transformed_to(transformation)
+      in_blueprint? ? super : super.select { |s| s.type == runtime_qualifier }
+    end
+
     def struct_merged_with(other); super.uniq(&:type) ;end
 
   end
