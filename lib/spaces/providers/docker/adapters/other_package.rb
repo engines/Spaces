03be0b2f45993cd296db19d59348bedb4aca518a
Mark Ratjens <mark@ratjens.com>
2021-10-25 18:08:18 +1100

    extract Adapters::Division
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/docker/adapters/other_package.rb
+++ lib/spaces/providers/docker/adapters/other_package.rb
@@ -6,12 +6,6 @@ module Adapters
         "RUN #{temporary_script_path}/#{qualifier}/add #{environment_vars.in_quotes.join(' ')}"
       end
 
-      def environment_vars
-        [:repository, :extraction, :extracted_path, :destination].map do |v|
-          division.send(v) if division.respond_to?(v)
-        end
-      end
-
     end
   end
 end
