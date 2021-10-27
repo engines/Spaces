e2cd7030ecae8484237302c77a8aae3f6263ce24
Mark Ratjens <mark@ratjens.com>
2021-10-27 11:50:30 +1100

    extract Snippets module from Artifact
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/models/transforming/artifacts/artifact.rb
+++ lib/spaces/models/transforming/artifacts/artifact.rb
@@ -1,49 +1,23 @@
+require_relative 'snippets'
+
 module Artifacts
   class Artifact < ::Spaces::Model
     include Adapters::Precedence
+    include Snippets
 
     relation_accessor :adapter
 
     delegate([:blueprint_identifier, :adapter_map, :adapter_keys] => :adapter)
 
-    alias_method :filename, :qualifier
-
     def value
       [snippets].flatten.join("\n") #TODO: is this a good default?
     end
 
-    def snippets
-      no_stanzas? ? snippets_here : stanzas.map(&:snippets)
+    def filename
+      "#{emission.identifier.as_path}.#{extension}"
     end
 
-    def snippets_here
-      precedence.map { |p| snippet_map[p] }.compact
-    end
-
-    def snippet_map
-      @snippet_map ||= adapters.reduce({}) do |m, a|
-        m.tap do
-          a.snippet_map.keys.map do |k|
-            p = precedence_for(k)
-            m[p] = [m[p], a.snippet_map[k]].flatten.compact
-          end
-        end
-      end
-    end
-
-    def snippet_keys; snippet_map.keys ;end
-
-    def no_stanzas?; stanza_qualifiers.empty? ;end
-
-    def stanza_qualifiers; [] ;end
-
-    def stanzas
-      @stanzas ||= stanza_qualifiers.map { |q| stanza_class_for(q).new(self) }
-    end
-
-    def stanza_class_for(qualifier)
-      class_for(nesting_elements, "#{qualifier}_stanza")
-    end
+    def extension; qualifier ;end
 
     def initialize(adapter)
       self.adapter = adapter
