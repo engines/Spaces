adedf525ff7ef26bce999cde27ce7928c434fdf6
Mark Ratjens <mark@ratjens.com>
2021-10-28 11:04:10 +1100

    FIX: stanza method_missing delegation
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/models/transforming/artifacts/artifact.rb
+++ lib/spaces/models/transforming/artifacts/artifact.rb
@@ -5,26 +5,28 @@ module Artifacts
     include Adapters::Precedence
     include Snippets
 
-    relation_accessor :adapter
+    relation_accessor :holder
+    alias_method :adapter, :holder
 
-    delegate([:blueprint_identifier, :adapter_map, :adapter_keys] => :adapter)
+    # delegate([:blueprint_identifier, :adapter_map, :adapter_keys] => :adapter)
+    delegate([:adapter_map, :adapter_keys] => :adapter)
 
     def value
       [snippets].flatten.join("\n") #TODO: is this a good default?
     end
 
-    def initialize(adapter)
-      self.adapter = adapter
+    def initialize(holder)
+      self.holder = holder
     end
 
     def method_missing(m, *args, &block)
       return adapter_map[m] if adapter_keys.include?(m)
-      return adapter.send(m, *args, &block) if adapter.respond_to?(m)
+      return holder.send(m, *args, &block) if holder.respond_to?(m)
       super
     end
 
     def respond_to_missing?(m, *)
-      adapter_map.include?(m) || adapter.respond_to?(m) || super
+      adapter_map.include?(m) || holder.respond_to?(m) || super
     end
 
   end
--- lib/spaces/models/transforming/artifacts/stanza.rb
+++ lib/spaces/models/transforming/artifacts/stanza.rb
@@ -3,15 +3,7 @@ require_relative 'artifact'
 module Artifacts
   class Stanza < Artifact
 
-    relation_accessor :artifact
-
-    delegate(
-      delegate([:blueprint_identifier, :division_adapter_keys, :division_adapters] => :artifact)
-    )
-
-    def initialize(artifact)
-      self.artifact = artifact
-    end
+    alias_method :artifact, :holder
 
     def method_missing(m, *args, &block)
       return artifact.send(m, *args, &block) if artifact.respond_to?(m)
