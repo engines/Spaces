e8430d4e490ea6a92fd181a4a2bbfd1533a04231
Mark Ratjens <mark@ratjens.com>
2021-11-24 12:07:29 +1100

    refine test scripts
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

---
+++ x/scripts_docker_database.rb
@@ -0,0 +1,37 @@
+# load the code!
+require './x/controllers'
+
+# destroy the universe
+# universe.workspace.rmtree
+
+# ------------------------------------------------------------------------------
+
+# import a database blueprint
+controllers.publishing.import(model: {repository: 'https://github.com/v2Blueprints/mariadb'}, threaded: false)
+
+# ------------------------------------------------------------------------------
+# save a database arena for common use
+controllers.arenas.create(model: {identifier: :database})
+
+# ------------------------------------------------------------------------------
+# bind some blueprints to the database arena
+controllers.arenas.bind(identifier: :database, blueprint_identifier: :mariadb)
+
+
+# save installations for the arena
+controllers.arenas.install(identifier: :database)
+
+# resolve the arena
+controllers.arenas.resolve(identifier: :database)
+
+# save all packs for the arena
+controllers.arenas.pack(identifier: :database)
+
+# provision the arena
+controllers.arenas.provision(identifier: :database)
+
+# # build packs for the arena
+# controllers.arenas.build(identifier: :database)
+#
+# # apply provisions for arena
+# controllers.arenas.apply(identifier: :database)
