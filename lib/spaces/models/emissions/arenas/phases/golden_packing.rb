e8cf3b9be24ce407bb7085d440d06a256abd72af
Mark Ratjens <mark@ratjens.com>
2021-12-10 14:30:51 +1100

    allow an arena to report identifiers for golden packs
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/models/emissions/arenas/phases/golden_packing.rb
+++ lib/spaces/models/emissions/arenas/phases/golden_packing.rb
@@ -1,6 +1,12 @@
 module Arenas
   module GoldenPacking
 
+    def golden_pack_identifiers
+      golden_image_divisions.map do |g|
+        packs.identifiers(blueprint_identifier: g.identifier)
+      end.flatten
+    end
+
     def golden_image_divisions
       all_image_divisions.select_uniq(&:potentially_golden?)
     end
