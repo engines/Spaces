bf91b427b361a344b99f7377094c736f7b2de153
Mark Ratjens <mark@ratjens.com>
2022-01-28 16:02:00 +1100

    Revert "add redmine to development arena script"
    This reverts commit b945ee14369c0e0c599087fb70b76e7b6b5c2104.

--- x/scripts_docker.rb
+++ x/scripts_docker.rb
@@ -9,7 +9,6 @@ require './x/controllers'
 # import some blueprints
 controllers.publishing.import(model: {repository: 'https://github.com/v2Blueprints/wap'}, threaded: false)
 controllers.publishing.import(model: {repository: 'https://github.com/v2Blueprints/phpmyadmin'}, threaded: false)
-controllers.publishing.import(model: {repository: 'https://github.com/v2Blueprints/redmine'}, threaded: false)
 
 # ------------------------------------------------------------------------------
 # blueprint indices, lists, and gets
@@ -20,8 +19,8 @@ controllers.blueprinting.list
 
 controllers.querying.list(method: :binder_identifiers, space: :blueprints)
 
-controllers.blueprinting.show(identifier: :redmine)
-controllers.blueprinting.summarize(identifier: :redmine)
+controllers.blueprinting.show(identifier: :phpmyadmin)
+controllers.blueprinting.summarize(identifier: :phpmyadmin)
 
 # ------------------------------------------------------------------------------
 
@@ -49,7 +48,6 @@ controllers.arenas.more_binders(identifier: :development)
 # bind some blueprints to the applications arena
 controllers.arenas.bind(identifier: :development, blueprint_identifier: :wap)
 controllers.arenas.bind(identifier: :development, blueprint_identifier: :phpmyadmin)
-controllers.arenas.bind(identifier: :development, blueprint_identifier: :redmine)
 controllers.arenas.state(identifier: :development)
 
 # resolve the arena
