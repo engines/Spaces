121fcd2c42da2f6f6c9472c90a08d98e17d5235d
Mark Ratjens <mark@ratjens.com>
2022-02-11 12:03:41 +1100

    CRUD free-standing Domains
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/framework/models/universe.rb
+++ lib/spaces/framework/models/universe.rb
@@ -24,7 +24,7 @@ module Spaces
           Arenas::Space.new(:arenas),
           Keys::Space.new(:user_keys),
 
-          Associations::Domains::Space.new(:domains)
+          Domains::Space.new(:domains)
         ]
       end
     end
---
+++ lib/spaces/models/associations/domains/space.rb
@@ -0,0 +1,11 @@
+module Domains
+  class Space < ::Spaces::Space
+
+    class << self
+      def default_model_class
+        Domain
+      end
+    end
+
+  end
+end
