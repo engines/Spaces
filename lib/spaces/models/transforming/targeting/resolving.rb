abe75e02c02e3d2c00617237b39b950719c58cc7
Mark Ratjens <mark@ratjens.com>
2022-02-15 15:45:04 +1100

    resolve service parameters again just before calling a service
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/models/transforming/targeting/resolving.rb
+++ lib/spaces/models/transforming/targeting/resolving.rb
@@ -8,6 +8,10 @@ module Targeting
       end
     end
 
+    def service_string_array
+      _resolved(:service)&.to_string_array
+    end
+
     def _resolved(section)
       if s = send(section)
         resolvable_struct_class.new(s, self).resolved
