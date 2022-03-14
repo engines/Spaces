e691ec3a085301d352ed8710438b89641b480550
Mark Ratjens <mark@ratjens.com>
2022-03-14 19:03:43 +1100

    test whether two arenas or blueprints are circular
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/models/transforming/targeting/tree_space.rb
+++ lib/spaces/models/transforming/targeting/tree_space.rb
@@ -1,6 +1,15 @@
 module Targeting
   class TreeSpace < ::Spaces::Space
 
+    def circular?(parent_identifier, candidate_child_identifier)
+      descendant_path_identifiers.map do |x|
+        [
+          x.index(parent_identifier.to_s),
+          x.index(candidate_child_identifier.to_s)
+        ].compact
+      end.any? { |x| x.count == 2 && x.first > x.last }
+    end
+
     def new_leaves_for(identifier:)
       identifiers - existing_nodes_for(identifier: identifier)
     end
