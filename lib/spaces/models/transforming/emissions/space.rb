6fb23a2f9726fb27354869d66a658e490621c120
Mark Ratjens <mark@ratjens.com>
2022-03-28 12:03:12 +1100

    FIX BUG: arenas space #identifiers returns file names inside arena directory
    it should return only the arena dir names
    
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/models/transforming/emissions/space.rb
+++ lib/spaces/models/transforming/emissions/space.rb
@@ -1,12 +1,6 @@
 module Emissions
   class Space < ::Spaces::PathSpace
 
-    def identifiers(arena_identifier: '*', blueprint_identifier: '*')
-      path.glob("#{arena_identifier}/#{blueprint_identifier}").map do |p|
-        "#{p.relative_path_from(path)}".as_compound
-      end
-    end
-
     def artifacts_for(identifier)
       if (m = exist_then_by(identifier))
         translator_for(m).artifacts
