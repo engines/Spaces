4fcf022d9f868b906dd2dab888af8bcb1934c98b
Mark Ratjens <mark@ratjens.com>
2022-04-06 13:11:36 +1000

    rename x files
    remove ‘scripts’ prefix
    
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- x/providers.rb
+++ x/providers.rb
@@ -7,3 +7,4 @@
 controllers.providing.create(model: {identifier: :docker_local, qualifier: :docker})
 controllers.providing.create(model: {identifier: :docker_compose_local, qualifier: :docker_compose})
 controllers.providing.create(model: {identifier: :terraform_local, qualifier: :terraform})
+controllers.providing.create(model: {identifier: :power_dns_local, qualifier: :power_dns})
