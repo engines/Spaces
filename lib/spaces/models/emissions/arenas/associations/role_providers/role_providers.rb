b4896933ccfd673e7787b61e1d3458bbde8b5885
Mark Ratjens <mark@ratjens.com>
2022-05-03 13:47:26 +1000

    teast whether an arena offers a provider for a given role
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/models/emissions/arenas/associations/role_providers/role_providers.rb
+++ lib/spaces/models/emissions/arenas/associations/role_providers/role_providers.rb
@@ -1,6 +1,10 @@
 module Associations
   class RoleProviders < ::Targeting::Tree
 
+    def identifiers
+      map(&:role_identifier)
+    end
+
     def named(role_identifier)
       all.detect { |rp| rp.role_identifier.to_sym == role_identifier.to_sym }
     end
--- lib/spaces/models/emissions/arenas/providing.rb
+++ lib/spaces/models/emissions/arenas/providing.rb
@@ -1,6 +1,10 @@
 module Arenas
   module Providing
 
+    def perform?(role_identifier)
+      role_providers.identifiers.include?(role_identifier.to_sym)
+    end
+
     def providers
       role_providers.map(&:provider)
     end
@@ -10,11 +14,11 @@ module Arenas
     end
 
     def qualifier_for(role_identifier)
-      provider_for(role_identifier).qualifier
+      provider_for(role_identifier)&.qualifier
     end
 
     def resolution_for(role_identifier)
-      provider_for(role_identifier).resolution
+      provider_for(role_identifier)&.resolution
     end
 
   end
