6c1e46e0ab35ff606a25b6003b681edc71ee7ea7
Mark Ratjens <mark@ratjens.com>
2022-05-04 18:15:47 +1000

    remove binding step from arena staging command
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/commands/arenas/staging.rb
+++ lib/spaces/commands/arenas/staging.rb
@@ -2,15 +2,10 @@ module Arenas
   module Commands
     class Staging < ::Spaces::Commands::Iterating
 
-      def blueprint_identifier
-        input_for(:blueprint_identifier)
-      end
-
       def mutating?; true ;end
 
       def subcommands
         @subcommands ||= [
-          Binding.new(subcommand_inputs.merge(blueprint_identifier: blueprint_identifier)),
           Resolving.new(subcommand_inputs),
           Packing.new(subcommand_inputs),
           Orchestrating.new(subcommand_inputs)
