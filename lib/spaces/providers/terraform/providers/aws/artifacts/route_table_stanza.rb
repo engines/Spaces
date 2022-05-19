e499a0b11b1f104b2d7351ae6c87df296c338285
Mark Ratjens <mark@ratjens.com>
2022-05-19 13:46:22 +1000

    refine aws route table stanzas
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/terraform/providers/aws/artifacts/route_table_stanza.rb
+++ lib/spaces/providers/terraform/providers/aws/artifacts/route_table_stanza.rb
@@ -7,25 +7,19 @@ module Artifacts
 
         def more_snippets
           %(
-            resource "aws_route" "public" {
-              route_table_id         = aws_route_table.public.id
-              destination_cidr_block = "0.0.0.0/0"
-              gateway_id             = aws_internet_gateway.aws-igw.id
+            route {
+              cidr_block = "10.111.111.0/24"
+              gateway_id = aws_internet_gateway.VPC_A-gw.id
             }
 
-            resource "aws_route_table_association" "publica" { arena binding_identifier!!!!!
-              subnet_id      = public-subnet-1
-              route_table_id = aws_route_table.public.id
+            route {
+              cidr_block = "10.111.222.0/24"
+              gateway_id = aws_internet_gateway.VPC_A-gw.id
             }
 
-            resource "aws_route_table_association" "publicb" {
-              subnet_id      = public-subnet-2
-              route_table_id = aws_route_table.public.id
-            }
-
-            resource "aws_route_table_association" "publicc" {
-              subnet_id      = public-subnet-3
-              route_table_id = aws_route_table.public.id
+            route {
+              cidr_block = "0.0.0.0/0"
+              gateway_id = aws_internet_gateway.VPC_A-gw.id
             }
           )
         end
