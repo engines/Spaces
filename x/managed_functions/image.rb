a2e2991adb11be47c7eee57fed095762325e31fe
Mark Ratjens <mark@ratjens.com>
2022-05-27 09:43:18 +1000

    define KmsStanza
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

---
+++ x/managed_functions/image.rb
@@ -0,0 +1,37 @@
+# load the code!
+# require './x/controllers'
+
+# ------------------------------------------------------------------------------
+
+# import a bootstrappy blueprint
+controllers.publishing.import(model: {repository: 'https://github.com/v2Blueprints/dougs-base-image'}, threaded: false)
+
+# ------------------------------------------------------------------------------
+
+# save a basic arena with default associations
+controllers.arenas.create(model: {identifier: :base})
+
+# ------------------------------------------------------------------------------
+
+# specify an image to build blueprints from
+controllers.arenas.build_from(identifier: :base, image_identifier: :'python:3.8-slim-buster')
+
+# ------------------------------------------------------------------------------
+
+# save some providers
+controllers.providing.create(model: {identifier: :docker, qualifier: :docker})
+
+# define packing providers
+controllers.arenas.provide(identifier: :base, role_identifier: :packing, provider_identifier: :docker)
+controllers.arenas.provide(identifier: :base, role_identifier: :runtime, provider_identifier: :docker)
+
+# ------------------------------------------------------------------------------
+
+# bind a base blueprint to the arena
+controllers.arenas.bind(identifier: :base, blueprint_identifier: :'dougs-base-image')
+
+# stage the arena for the bindings -- should not orchestrate!
+controllers.arenas.stage(identifier: :base)
+
+# # build images for the arena
+# controllers.arenas.build(identifier: :base, threaded: false)
