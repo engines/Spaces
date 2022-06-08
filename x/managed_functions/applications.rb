9c4c55ce1bb9ec36ed11b3fa2cca2f562d3401a3
Mark Ratjens <mark@ratjens.com>
2022-06-08 12:55:13 +1000

    refine managed functions test scripts
    add testing for registry
    
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- x/managed_functions/applications.rb
+++ x/managed_functions/applications.rb
@@ -9,33 +9,33 @@ controllers.publishing.import(model: {repository: 'https://github.com/EnginesAWS
 
 # ------------------------------------------------------------------------------
 # save a providers arena for mf_infrastructure use
-controllers.arenas.create(model: {identifier: :managed_functions})
+controllers.arenas.create(model: {identifier: :applications})
 
 # specify an image to build blueprints from
-controllers.arenas.build_from(identifier: :managed_functions, image_identifier: :base_python)
+controllers.arenas.build_from(identifier: :applications, image_identifier: :base_python)
 
 # ------------------------------------------------------------------------------
 
 # save some providers
 controllers.providing.create(model: {identifier: :docker, qualifier: :docker})
 controllers.providing.create(model: {identifier: :terraform, qualifier: :terraform})
-controllers.providing.create(model: {identifier: :aws, qualifier: :aws})
+controllers.providing.create(model: {identifier: :aws, qualifier: :aws, region: :'ap-southeast-2'})
 
 # define role providers
-controllers.arenas.provide(identifier: :managed_functions, role_identifier: :packing, provider_identifier: :docker)
-controllers.arenas.provide(identifier: :managed_functions, role_identifier: :orchestration, provider_identifier: :terraform, compute_identifier: :aws)
-controllers.arenas.provide(identifier: :managed_functions, role_identifier: :runtime, provider_identifier: :docker)
+controllers.arenas.provide(identifier: :applications, role_identifier: :packing, provider_identifier: :docker)
+controllers.arenas.provide(identifier: :applications, role_identifier: :orchestration, provider_identifier: :terraform, compute_identifier: :aws)
+controllers.arenas.provide(identifier: :applications, role_identifier: :runtime, provider_identifier: :docker)
 
 # bind blueprints to the arena
-controllers.arenas.bind(identifier: :managed_functions, blueprint_identifier: 'managed-functions')
-controllers.arenas.bind(identifier: :managed_functions, blueprint_identifier: 'startup-infrastructure')
+controllers.arenas.bind(identifier: :applications, blueprint_identifier: 'managed-functions')
+controllers.arenas.bind(identifier: :applications, blueprint_identifier: 'startup-infrastructure')
 
 # ------------------------------------------------------------------------------
 # stage some blueprints in the mf_infrastructure arena
-controllers.arenas.stage(identifier: :managed_functions)
+controllers.arenas.stage(identifier: :applications)
 
 # # build images for the arena
-# controllers.arenas.build(identifier: :managed_functions, threaded: false)
+# controllers.arenas.build(identifier: :applications, threaded: false)
 #
 # # bring up containers for arena
-# controllers.arenas.apply(identifier: :managed_functions, threaded: false)
+# controllers.arenas.apply(identifier: :applications, threaded: false)
