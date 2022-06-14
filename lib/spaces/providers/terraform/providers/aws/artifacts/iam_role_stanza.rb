4869a67645fb6ea3a60b1b08eb17a24c946caeab
Mark Ratjens <mark@ratjens.com>
2022-06-14 16:41:02 +1000

    refine iam role stanza
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

---
+++ lib/spaces/providers/terraform/providers/aws/artifacts/iam_role_stanza.rb
@@ -0,0 +1,30 @@
+require_relative 'capsule_stanza'
+
+module Artifacts
+  module Terraform
+    module Aws
+      class IamRoleStanza < CapsuleStanza
+
+        def more_snippets
+          %(
+              assume_role_policy = jsonencode({
+                Version = "2012-10-17"
+                Statement = [
+                  {
+                    Action = "sts:AssumeRole"
+                    Effect = "Allow"
+                    Sid    = ""
+                    Principal = {
+                      Service = "ec2.amazonaws.com"
+                    }
+                  }
+                ]
+              })
+            }
+          )
+        end
+
+      end
+    end
+  end
+end
