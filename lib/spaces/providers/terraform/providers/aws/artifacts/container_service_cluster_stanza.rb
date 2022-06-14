718e9fa3b1a6a0f2bdd0256bdfc68a234774f0a7
Mark Ratjens <mark@ratjens.com>
2022-06-14 16:03:54 +1000

    for now, drop container service configuration snippet
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/terraform/providers/aws/artifacts/container_service_cluster_stanza.rb
+++ lib/spaces/providers/terraform/providers/aws/artifacts/container_service_cluster_stanza.rb
@@ -9,18 +9,6 @@ module Artifacts
           %(
             resource "aws_#{resource_type}" "#{application_identifier}" {
               name = "#{application_identifier}"
-
-              configuration {
-                execute_command_configuration {
-                  kms_key_id = #{configuration&.kms_key_id}
-                  logging    = "#{configuration&.logging}"
-
-                  log_configuration {
-                    cloud_watch_encryption_enabled = #{configuration&.logging_encryption_enabled}
-                    cloud_watch_log_group_name     = #{configuration&.log_group_name}
-                  }
-                }
-              }
             }
           )
         end
