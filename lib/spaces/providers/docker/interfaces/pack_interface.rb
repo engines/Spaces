a667c35090b297e61effcc5e6e81b95cfc34629d
James Vodanovich <james@jvodan.com>
2022-07-01 10:11:10 +1000

    respository -> repository

--- lib/spaces/providers/docker/interfaces/pack_interface.rb
+++ lib/spaces/providers/docker/interfaces/pack_interface.rb
@@ -9,7 +9,7 @@ module Providers
       delegate(
         packs: :universe,
         path_for: :packs,
-        [:packing_compute_provider, :packing_compute_respository_path] => :arena
+        [:packing_compute_provider, :packing_compute_repository_path] => :arena
       )
 
       def build
@@ -21,7 +21,7 @@ module Providers
       def build_from_pack
         build_from_dir.tap do |i|
           if packing_compute_provider
-            i.tag(repo: packing_compute_respository_path, tag: pack.output_identifier, force: true)
+            i.tag(repo: packing_compute_repository_path, tag: pack.output_identifier, force: true)
           else
             i.tag(repo: pack.output_identifier, tag: default_tag, force: true)
           end
