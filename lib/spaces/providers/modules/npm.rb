153444d9c2b98dfb02159d915adc17b9d0c6432e
Mark Ratjens <mark@ratjens.com>
2022-07-06 16:25:49 +1000

    use endless defs in modules
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/modules/npm.rb
+++ lib/spaces/providers/modules/npm.rb
@@ -1,9 +1,7 @@
 module Providers
   class Npm < ::Adapters::ModuleList
 
-    def inline
-      struct.map { |s| "npm install #{s}" }
-    end
+    def inline = struct.map { |s| "npm install #{s}" }
 
   end
 end
