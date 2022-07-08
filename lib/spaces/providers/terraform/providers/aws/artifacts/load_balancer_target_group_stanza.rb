1f8ed047caf9464f3e426b23226ccac54c2e351d
Mark Ratjens <mark@ratjens.com>
2022-07-08 10:34:22 +1000

    FIX: merge conflict stuff left in!
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/terraform/providers/aws/artifacts/load_balancer_target_group_stanza.rb
+++ lib/spaces/providers/terraform/providers/aws/artifacts/load_balancer_target_group_stanza.rb
@@ -4,23 +4,18 @@ module Artifacts
   module Terraform
     module Aws
       class LoadBalancerTargetGroupStanza < ResourceStanza
-<<<<<<< HEAD
+
+    		def configuration_snippet =
+          %(
+            vpc_id = aws_vpc.#{configuration.vpc_binding}.id
+            subnets = aws_subnet.public.*.id
+            security_groups = [aws_security_group.lb.id]
+            protocol = #{configuration.protocol}
+            port = #{configuration.port}
+          )
 
         def more_snippets =
-=======
-		def configuration_snippet
-			%(
-             vpc_id = aws_vpc.#{configuration.vpc_binding}.id
-			 subnets         = aws_subnet.public.*.id
-  			 security_groups = [aws_security_group.lb.id]
-             protocol = #{configuration.protocol}
-             port = #{configuration.port}
-            )
-		end
-        def more_snippets
->>>>>>> 6a416912a68f35c58e7c874a190c5397a9ec2aae
           %(
-			
             health_check {
               healthy_threshold   = #{configuration.healthy_threshold}
               interval            = #{configuration.interval}
@@ -31,25 +26,21 @@ module Artifacts
               unhealthy_threshold = #{configuration.unhealthy_threshold}
             }
           )
-<<<<<<< HEAD
 
-=======
-        end
-        def default_configuration
+        def default_configuration =
           OpenStruct.new(
             description: application_identifier,
-				target_type: "ip",
-				healthy_threshold: 3,		
-      			interval: 300,
-      			protocol: 'HTTP',
-      			matcher: 200,
-      			timeout: 3,
-				unhealthy_threshold: 2,
-				health_check_path: '/',				
-			    path_pattern:  ["/*"]
+            target_type: "ip",
+            healthy_threshold: 3,
+            interval: 300,
+            protocol: 'HTTP',
+            matcher: 200,
+            timeout: 3,
+            unhealthy_threshold: 2,
+            health_check_path: '/',
+            path_pattern:  ["/*"]
           )
-        end
->>>>>>> 6a416912a68f35c58e7c874a190c5397a9ec2aae
+
       end
     end
   end
