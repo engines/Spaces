4b8a06e7c0b43e8cfb290b4c8ebd3215ca3c5e6d
Mark Ratjens <mark@ratjens.com>
2022-07-28 13:18:24 +1000

    define precondition against deleting a provider used in an arena
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/models/transforming/providers/space.rb
+++ lib/spaces/models/transforming/providers/space.rb
@@ -9,5 +9,11 @@ module Providers
 
     def application_qualifiers = default_model_class.application_qualifiers
 
+    def deletion_precondition(identifiable:)
+      {
+        unused_in_arena: !exist_then_by(identifiable)&.used_in_arena?
+      }
+    end
+
   end
 end
