487f392e597c3ab39f97f2ba9cd6537dc81d4fe5
Mark Ratjens <mark@ratjens.com>
2022-07-28 14:34:13 +1000

    standardise port division keys
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/models/divisions/ports/port.rb
+++ lib/spaces/models/divisions/ports/port.rb
@@ -2,15 +2,15 @@ module Divisions
   class Port < ::Divisions::Subdivision
 
     class << self
-      def features = [:start_port, :end_port, :protocol, :external_port]
+      def features = [:container_port, :end_port, :protocol, :host_port]
     end
 
     def command =
-      "/usr/local/bin/open_port.sh -h #{address} -p #{protocol} -e #{external_port} -s #{port_range}"
+      "/usr/local/bin/open_port.sh -h #{address} -p #{protocol} -e #{host_port} -s #{port_range}"
 
     def address = arena&.configuration&.address
 
-    def port_range = [start_port, end_port].compact.join(':')
+    def port_range = [container_port, end_port].compact.join(':')
     def end_port = struct.end_port
 
   end
