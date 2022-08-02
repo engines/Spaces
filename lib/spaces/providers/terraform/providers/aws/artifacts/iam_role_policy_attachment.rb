23eb0a7788d0923cb3484809fa58dcd7af032fa0
James Vodanovich <james@jvodan.com>
2022-08-02 14:51:40 +1000

    no tags pn policy assignments

--- lib/spaces/providers/terraform/providers/aws/artifacts/iam_role_policy_attachment.rb
+++ lib/spaces/providers/terraform/providers/aws/artifacts/iam_role_policy_attachment.rb
@@ -4,13 +4,14 @@ module Artifacts
   module Terraform
     module Aws
       class IamRolePolicyAttachmentStanza < ResourceStanza
-
         def configuration_snippet =
           %(
             role       = "#{configuration.role}"
             policy_arn = "#{configuration.role_arn}"
           )
+      end
 
+      def tags_snippet
       end
     end
   end
