8e332e51c4ac849e40117bc21e2d78eeee88c2ab
Mark Ratjens <mark@ratjens.com>
2022-08-03 13:06:31 +1000

    FIX BUG: #tags_snippet defined in wrong module scope
    of Aws:: IamRolePolicyAttachmentStanza
    
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/terraform/providers/aws/artifacts/iam_role_policy_attachment.rb
+++ lib/spaces/providers/terraform/providers/aws/artifacts/iam_role_policy_attachment.rb
@@ -4,14 +4,15 @@ module Artifacts
   module Terraform
     module Aws
       class IamRolePolicyAttachmentStanza < ResourceStanza
+
         def configuration_snippet =
           %(
             role       = "#{configuration.role}"
             policy_arn = "#{configuration.role_arn}"
           )
-      end
 
-      def tags_snippet
+        def tags_snippet = nil
+
       end
     end
   end
