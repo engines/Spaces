9274484005ce658db3d5b7330c04d36a7efdfd97
Mark Ratjens <mark@ratjens.com>
2022-08-03 13:36:24 +1000

    refine IamRolePolicyAttachmentStanza
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/terraform/providers/aws/artifacts/iam_role_policy_attachment.rb
+++ lib/spaces/providers/terraform/providers/aws/artifacts/iam_role_policy_attachment.rb
@@ -5,10 +5,17 @@ module Artifacts
     module Aws
       class IamRolePolicyAttachmentStanza < ResourceStanza
 
+        class << self
+          def default_configuration =
+            OpenStruct.new(
+              role_binding: :'iam-role'
+            )
+        end
+
         def configuration_snippet =
           %(
-            role       = "#{configuration.role}"
-            policy_arn = "#{configuration.role_arn}"
+            role       = aws_iam_role.#{configuration.role_binding}.name
+            policy_arn = aws_iam_role.#{configuration.role_binding}.arn
           )
 
         def tags_snippet = nil
