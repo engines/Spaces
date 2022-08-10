0484d23193b3f04cce07f3cd8d59a258eb088f69
James Vodanovich <james@jvodan.com>
2022-08-10 15:29:14 +1000

    add depends

--- lib/spaces/providers/terraform/providers/aws/artifacts/stanzas/load_balancer.rb
+++ lib/spaces/providers/terraform/providers/aws/artifacts/stanzas/load_balancer.rb
@@ -4,11 +4,11 @@ module Artifacts
   module Terraform
     module Aws
       class LoadBalancerStanza < ResourceStanza
-
         def configuration_snippet =
           %(
             subnets         = #{subnet_array}
             security_groups = #{security_group_array}
+			#{depends_on_snippet}
           )
 
         def subnet_array =
@@ -21,6 +21,13 @@ module Artifacts
             "aws_security_group.#{arena_resource_qualification_for(s)}.id"
           end
 
+        def depends_on_snippet =
+          configuration.security_groups.map do |s|
+            "aws_security_group.#{arena_resource_qualification_for(s)}"
+          end
+        configuration.subnets.map do |s|
+          "aws_subnet.#{arena_resource_qualification_for(s)}"
+        end
       end
     end
   end
