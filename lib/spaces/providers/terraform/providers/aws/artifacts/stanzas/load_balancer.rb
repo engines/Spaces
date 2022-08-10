76d9d94f5dfdc42f14f4e73a00c40bf2ffb722a7
James Vodanovich <james@jvodan.com>
2022-08-10 15:36:48 +1000

    ruby 3 syntax ?

--- lib/spaces/providers/terraform/providers/aws/artifacts/stanzas/load_balancer.rb
+++ lib/spaces/providers/terraform/providers/aws/artifacts/stanzas/load_balancer.rb
@@ -6,29 +6,35 @@ module Artifacts
       class LoadBalancerStanza < ResourceStanza
         def configuration_snippet =
           %(
-            subnets         = #{subnet_array}
-            security_groups = #{security_group_array}
+            subnets         = #{subnet_array_ids}
+            security_groups = #{security_group_array_ids}
 			#{depends_on_snippet}
           )
 
-        def subnet_array =
+        def subnet_array_ids =
           configuration.subnets.map do |s|
             "aws_subnet.#{arena_resource_qualification_for(s)}.id"
           end
 
-        def security_group_array =
+        def security_group_array_ids =
           configuration.security_groups.map do |s|
             "aws_security_group.#{arena_resource_qualification_for(s)}.id"
           end
-      end
+  
+         def subnet_array =
+          configuration.subnets.map do |s|
+            "aws_subnet.#{arena_resource_qualification_for(s)}"
+          end
 
-      def depends_on_snippet
-        configuration.security_groups.map do |s|
-          "aws_security_group.#{arena_resource_qualification_for(s)}"
-        end
-        configuration.subnets.map do |s|
-          "aws_subnet.#{arena_resource_qualification_for(s)}"
-        end
+        def security_group_array =
+          configuration.security_groups.map do |s|
+            "aws_security_group.#{arena_resource_qualification_for(s)}"
+          end
+  
+
+      def depends_on_snippet =
+		"depends_on[#{security_group_array.merge(subnet_array)}]"		
+       
       end
     end
   end
