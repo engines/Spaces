0cd5f00d30fc127eeb2daeeb6135869f6b7c87d1
Mark Ratjens <mark@ratjens.com>
2022-08-10 11:44:24 +1000

    refine container service default configuration
    is iam_role compatible with network_mode of ‘awsvpc’
    
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/terraform/providers/aws/artifacts/stanzas/container_service.rb
+++ lib/spaces/providers/terraform/providers/aws/artifacts/stanzas/container_service.rb
@@ -11,19 +11,20 @@ module Artifacts
           def default_configuration =
             super.merge(
               cluster_binding: :'container-service-cluster',
-              iam_role_binding: :'iam-role',
-              task_definition_binding: :'container-task-definition',
-              target_group_binding: :'load_balancer_target_group',
+              # iam_role_binding: :'iam-role',
               launch_type: :'FARGATE'
             )
 
           def launch_type = default_configuration.launch_type
         end
 
+        def default_configuration =
+          super.merge(
+            task_definition_binding: :"#{blueprint_identifier.hyphenated}",
+            target_group_binding: :"#{blueprint_identifier.hyphenated}"
         def more_snippets =
           %(
             cluster = aws_ecs_cluster.#{arena_attachable_qualification_for(:cluster_binding)}.id
-            iam_role = aws_iam_role.#{arena_attachable_qualification_for(:iam_role_binding)}.arn
             task_definition = aws_ecs_task_definition.#{arena_attachable_qualification_for(:task_definition_binding)}.arn
             ordered_placement_strategy {
               type  = "binpack"
@@ -35,9 +36,24 @@ module Artifacts
               container_port   = "#{ports.first.container_port}"
             }
           )
+          # %(
+          #   cluster = aws_ecs_cluster.#{arena_attachable_qualification_for(:cluster_binding)}.id
+          #   iam_role = aws_iam_role.#{arena_attachable_qualification_for(:iam_role_binding)}.arn
+          #   task_definition = aws_ecs_task_definition.#{arena_attachable_qualification_for(:task_definition_binding)}.arn
+          #   ordered_placement_strategy {
+          #     type  = "binpack"
+          #     field = "cpu"
+          #   }
+          #   load_balancer {
+          #     target_group_arn = aws_lb_target_group.#{arena_attachable_qualification_for(:target_group_binding)}.arn
+          #     container_name   = "#{application_identifier}"
+          #     container_port   = "#{ports.first.container_port}"
+          #   }
+          # )
 
         def configuration_hash =
-          super.without(:cluster_binding, :iam_role_binding, :task_definition_binding, :target_group_binding)
+          super.without(:cluster_binding, :task_definition_binding, :target_group_binding)
+          # super.without(:cluster_binding, :iam_role_binding, :task_definition_binding, :target_group_binding)
 
       end
     end
