396728e43215e63802c1599027d1732ac9452b6b
James Vodanovich <james@jvodan.com>
2022-08-10 15:49:28 +1000

    missing ""

--- lib/spaces/providers/terraform/providers/aws/artifacts/stanzas/iam_role_policy_attachment.rb
+++ lib/spaces/providers/terraform/providers/aws/artifacts/stanzas/iam_role_policy_attachment.rb
@@ -15,7 +15,7 @@ module Artifacts
         def configuration_snippet =
           %(
             role       = aws_iam_role.#{arena_attachable_qualification_for(:role_binding)}.id
-            policy_arn = arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole
+            policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
           )
 
         def tags_snippet = nil
