7b73cc2ff0b8555dc989f5a466e01ae25fc7f874
Mark Ratjens <mark@ratjens.com>
2022-08-12 14:54:10 +1000

    aws iam role policy (alternative)
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

---
+++ lib/spaces/providers/terraform/providers/aws/artifacts/stanzas/iam_role_policy.rb
@@ -0,0 +1,39 @@
+require_relative 'resource'
+
+module Artifacts
+  module Terraform
+    module Aws
+      class IamRolePolicyStanza < ResourceStanza
+        include Named
+
+        class << self
+          def default_configuration =
+            super.merge(
+              role_binding: :'iam-role'
+            )
+        end
+
+        def configuration_snippet =
+          %(
+            role = aws_iam_role.#{arena_attachable_qualification_for(:role_binding)}.id
+
+            policy = jsonencode({
+              Version = "2012-10-17"
+              Statement = [
+                {
+                  Action = [
+                    "ec2:Describe*",
+                  ]
+                  Effect   = "Allow"
+                  Resource = "*"
+                },
+              ]
+            })
+          )
+
+        def tags_snippet = nil
+
+      end
+    end
+  end
+end
