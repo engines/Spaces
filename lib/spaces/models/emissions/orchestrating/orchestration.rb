bd006ff85520bcb885c27245efa9504fec0d38ed
Mark Ratjens <mark@ratjens.com>
2022-08-18 15:34:36 +1000

    remove commented-out composition class def
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/models/emissions/orchestrating/orchestration.rb
+++ lib/spaces/models/emissions/orchestrating/orchestration.rb
@@ -1,10 +1,6 @@
 module Orchestrating
   class Orchestration < ::Resolving::Emission
 
-    # class << self
-    #   def composition_class = Composition
-    # end
-
     def connections_orchestrated = connections_down.map(&:orchestrated)
 
   end
