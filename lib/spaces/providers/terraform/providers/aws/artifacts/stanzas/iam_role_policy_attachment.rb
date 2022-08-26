3dc79bfe49d0d906a2d8bf404450df6975061018
Mark Ratjens <mark@ratjens.com>
2022-08-26 13:58:59 +1000

    FIX: duplicated snippets for role policy attachments
    iam_role
    policy_arn
    
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/terraform/providers/aws/artifacts/stanzas/iam_role_policy_attachment.rb
+++ lib/spaces/providers/terraform/providers/aws/artifacts/stanzas/iam_role_policy_attachment.rb
@@ -7,18 +7,18 @@ module Artifacts
 
         class << self
           def default_configuration =
+            #FIX: use terraform HCL instead of AWS notation
+            # why did the following not work?
+            # aws_iam_role.#{arena_attachable_qualification_for(:role_binding)}.arn
             super.merge(
-              role_binding: :'iam-role'
+              role_binding: :'iam-role',
+              policy_arn: "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
             )
         end
 
         def more_snippets =
-          #FIX: use terraform HCL instead of AWS notation
-          # why did the following not work?
-          # aws_iam_role.#{arena_attachable_qualification_for(:role_binding)}.arn
           %(
-            role       = aws_iam_role.#{arena_attachable_qualification_for(:role_binding)}.id
-            policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+            role = aws_iam_role.#{arena_attachable_qualification_for(:role_binding)}.id
           )
 
         def tags_snippet = nil
