72c87d8bc2dbaa6f5d54b7d70fde77391a04c241
Mark Ratjens <mark@ratjens.com>
2022-08-29 14:38:38 +1000

    derive default load balancer subnets and security groups
    from explicitly declared resources for the arena
    
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/terraform/providers/aws/artifacts/stanzas/load_balancer.rb
+++ lib/spaces/providers/terraform/providers/aws/artifacts/stanzas/load_balancer.rb
@@ -5,16 +5,7 @@ module Artifacts
     module Aws
       class LoadBalancerStanza < ResourceStanza
 
-        class << self
-          def default_configuration =
-            super.merge(
-              subnets: [],
-              security_groups: []
-            )
-        end
-
-        def subnet_identifiers = subnet_array.map { |s| "#{s}.id"}
-        def security_group_identifiers = security_group_array.map { |g| "#{g}.id"}
+        delegate(resources_by: :arena)
 
         def more_snippets =
           %(
@@ -24,6 +15,9 @@ module Artifacts
           )
 
         def more_snippets_keys = [:subnets, :security_groups]
+
+        def subnet_identifiers = subnet_array.map { |s| "#{s}.id" }
+        def security_group_identifiers = security_group_array.map { |g| "#{g}.id" }
 
         def dependency_array = [security_group_array, subnet_array].flatten
 
@@ -36,6 +30,18 @@ module Artifacts
           configuration.security_groups.map do |s|
             "aws_security_group.#{arena_resource_qualification_for(s)}"
           end
+
+        def default_configuration =
+          super.merge(
+            subnets: default_subnets,
+            security_groups: default_security_groups
+          )
+
+        def default_subnets =
+          resources_by(:subnet).select { |s| s.configuration.public }.map(&:identifier)
+
+        def default_security_groups =
+          resources_by(:security_group).map(&:identifier)
 
       end
     end
