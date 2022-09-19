61b94880509baffef277ebb17c025b1a840a3d87
Mark Ratjens <mark@ratjens.com>
2022-09-19 16:27:55 +1000

    prettify hcl artifacts
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/terraform/artifacts/artifact.rb
+++ lib/spaces/providers/terraform/artifacts/artifact.rb
@@ -13,6 +13,31 @@ module Artifacts
       def container_type =
         [runtime_qualifier, 'container'].compact.join('_')
 
+      def content = super.as_prettier_hcl
+
     end
   end
+end
+
+
+class String
+
+  def as_prettier_hcl
+    opener = /[({\[]/
+    closer = /[)}\]]/
+    indent_level = 0
+
+    split("\n").map(&:strip).reject(&:empty?).join("\n").gsub('resource ', "\nresource ").chars.map do |c|
+      case c
+      when opener
+        indent_level += 1
+      when closer
+        indent_level -= 1
+      when "\n"
+        c = "#{c}#{' ' * (indent_level * 2)}"
+      end
+      c
+    end.join
+  end
+
 end
