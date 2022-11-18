41a42304586509efea39d4b49d59e33e014c7057
Mark Ratjens <mark@ratjens.com>
2022-11-18 17:49:23 +1100

    allow Repository to delegate to a package accessor
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- lib/spaces/providers/aws/code_artifact/repository.rb
+++ lib/spaces/providers/aws/code_artifact/repository.rb
@@ -2,31 +2,37 @@ module Adapters
   module CodeArtifact
     class Repository < ::Adapters::Repository
 
+      class << self
+        def format_map =
+          {
+            python: :pypi,
+            node: :npm
+          }
+      end
+
       delegate(
+        format_map: :klass,
         struct: :division,
-        [:domain, :name] => :struct,
+        [:domain, :name, :content] => :struct,
         [:account_identifier, :region] => :compute_provider
       )
 
-      def access
-        system_execute(
-          configure_global_index_url(
-            system_execute(get_authorization_token).first.chop
-          )
-        )
+      def compute_provider =
+        @computer_provider ||= arena.compute_provider_for_role(:packing)
+
+      def index_url
+        "https://aws:#{token}@#{domain}-#{account_identifier}.d.codeartifact.#{region}.amazonaws.com/#{format_map[content]}/#{name}/simple/)"
       end
 
-      def configure_global_index_url(token)
-        #FIX: assumes the repository contains python! will need to delegate to a Providers::Python object
-        %(XDG_CONFIG_HOME=/tmp pip3 config set global.index-url https://aws:#{token}@#{domain}-#{account_identifier}.d.codeartifact.#{region}.amazonaws.com/pypi/#{name}/simple/)
+      def token
+        @token ||= system_execute(get_authorization_token).first.chop
       end
+
+      protected
 
       def get_authorization_token
         %(aws codeartifact get-authorization-token --domain #{domain} --domain-owner #{account_identifier} --region #{region} --query authorizationToken --output text --duration-seconds 900)
       end
-
-      def compute_provider =
-        @computer_provider ||= arena.compute_provider_for_role(:packing)
 
     end
   end
