f01f002768380bb1034312952111a6dd8216f775
Mark Ratjens <mark@ratjens.com>
2022-12-07 14:56:45 +1100

    remove commented-out terraform adapters

--- lib/spaces/providers/terraform/adapters/adapters.rb
+++
@@ -1,9 +0,0 @@
-module Adapters
-  module Terraform
-
-    # class Dimensions < ::Adapters::Dimensions ;end
-    # class Volumes < ::Adapters::Volumes ;end
-    # class Port < ::Adapters::Port ;end
-
-  end
-end
