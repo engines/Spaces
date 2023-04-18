07efab3a1127831a63f83123d00cf1781faa4288
Lachlan Douglas <lachlan@engines.org>
2023-04-18 12:00:41 +0800

    redefine patches after git gem update

--- lib/gems/git/git.rb
+++ lib/gems/git/git.rb
@@ -1,7 +1,9 @@
 module Git
 
-  def self.clone(repository, name, options = {}, &block)
-    Base.clone(repository, name, options, &block)
+  def self.clone(repository_url, directory = nil, options = {}, &block)
+    clone_to_options = options.select { |key, _value| %i[bare mirror].include?(key) }
+    directory ||= Git::URL.clone_to(repository_url, **clone_to_options)
+    Base.clone(repository_url, directory, options, &block)
   end
 
 end
--- lib/gems/git/git/lib.rb
+++ lib/gems/git/git/lib.rb
@@ -10,38 +10,43 @@ module Git
     #  Add &block as argument for passing down to command.
     #  Add opts argument
     #  Add --verbose and --progress options.
-    def pull(remote='origin', branch='master', opts = {}, &block)
+    def pull(remote = nil, branch = nil, opts = {}, &block)
+      raise ArgumentError, "You must specify a remote if a branch is specified" if remote.nil? && !branch.nil?
+
       arr_opts = []
+      arr_opts << remote if remote
+      arr_opts << branch if branch
       arr_opts << "--verbose" if opts[:verbose]
       arr_opts << "--progress" if opts[:progress]
 
-      command('pull', remote, branch, arr_opts, &block)
+      command('pull', *arr_opts, &block)
     end
 
     # Redefine existing method.
     #  Add &block as argument for passing down to command.
     #  Add --verbose and --progress options.
-    def clone(repository, name, opts = {}, &block)
+    def clone(repository_url, directory, opts = {}, &block)
       @path = opts[:path] || '.'
-      clone_dir = opts[:path] ? File.join(@path, name) : name
+      clone_dir = opts[:path] ? File.join(@path, directory) : directory
 
       arr_opts = []
       arr_opts << '--bare' if opts[:bare]
       arr_opts << '--branch' << opts[:branch] if opts[:branch]
       arr_opts << '--depth' << opts[:depth].to_i if opts[:depth] && opts[:depth].to_i > 0
-      arr_opts << '--config' << opts[:config] if opts[:config]
+      arr_opts << '--filter' << opts[:filter] if opts[:filter]
+      Array(opts[:config]).each { |c| arr_opts << '--config' << c }
       arr_opts << '--origin' << opts[:remote] || opts[:origin] if opts[:remote] || opts[:origin]
       arr_opts << '--recursive' if opts[:recursive]
-      arr_opts << "--mirror" if opts[:mirror]
+      arr_opts << '--mirror' if opts[:mirror]
       arr_opts << "--verbose" if opts[:verbose]
       arr_opts << "--progress" if opts[:progress]
 
       arr_opts << '--'
 
-      arr_opts << repository
+      arr_opts << repository_url
       arr_opts << clone_dir
 
-      command('clone', arr_opts, &block)
+      command('clone', *arr_opts, &block)
 
       return_base_opts_from_clone(clone_dir, opts)
     end
@@ -62,23 +67,43 @@ module Git
       arr_opts << "--verbose" if opts[:verbose]
       arr_opts << "--progress" if opts[:progress]
 
-      if opts[:gpg_sign]
+      if opts[:gpg_sign] && opts[:no_gpg_sign]
+        raise ArgumentError, 'cannot specify :gpg_sign and :no_gpg_sign'
+      elsif opts[:gpg_sign]
         arr_opts <<
-        if opts[:gpg_sign] == true
-          '--gpg-sign'
-        else
-          "--gpg-sign=#{opts[:gpg_sign]}"
-        end
+          if opts[:gpg_sign] == true
+            '--gpg-sign'
+          else
+            "--gpg-sign=#{opts[:gpg_sign]}"
+          end
+      elsif opts[:no_gpg_sign]
+        arr_opts << '--no-gpg-sign'
       end
 
-      command('commit', arr_opts, &block)
+      command('commit', *arr_opts, &block)
     end
+
 
     # Redefine existing method.
     #  Add &block as argument for passing down to command.
     #  Add --verbose and --progress options.
-    def push(remote, branch = 'master', opts = {}, &block)
+    def push(remote = nil, branch = nil, opts = nil, &block)
+      if opts.nil? && branch.instance_of?(Hash)
+        opts = branch
+        branch = nil
+      end
+
+      if opts.nil? && remote.instance_of?(Hash)
+        opts = remote
+        remote = nil
+      end
+
+      opts ||= {}
+
+      # Small hack to keep backwards compatibility with the 'push(remote, branch, tags)' method signature.
       opts = {:tags => opts} if [true, false].include?(opts)
+
+      raise ArgumentError, "You must specify a remote if a branch is specified" if remote.nil? && !branch.nil?
 
       arr_opts = []
       arr_opts << '--mirror'  if opts[:mirror]
@@ -86,13 +111,16 @@ module Git
       arr_opts << '--force'  if opts[:force] || opts[:f]
       arr_opts << "--verbose" if opts[:verbose]
       arr_opts << "--progress" if opts[:progress]
-      arr_opts << remote
+      Array(opts[:push_option]).each { |o| arr_opts << '--push-option' << o } if opts[:push_option]
+      arr_opts << remote if remote
+      arr_opts_with_branch = arr_opts.dup
+      arr_opts_with_branch << branch if branch
 
       if opts[:mirror]
-          command('push', arr_opts, &block)
+          command('push', *arr_opts_with_branch, &block)
       else
-          command('push', arr_opts + [branch], &block)
-          command('push', ['--tags'] + arr_opts, &block) if opts[:tags]
+          command('push', *arr_opts_with_branch, &block)
+          command('push', '--tags', *arr_opts, &block) if opts[:tags]
       end
     end
 
